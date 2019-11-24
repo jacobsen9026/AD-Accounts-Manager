ec9bb3aafe56d29b55155b77b34bbb34cdb85d84
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-24 16:15:11 -0500

    Create configController

--- utils/fileFunctions.php
+++ utils/fileFunctions.php
@@ -21,6 +21,25 @@ function recurse_copy($src,$dst) {
     closedir($dir); 
 }
 
+function auditLogon($username){
+	$dir="./logs/";
+	$file="./logs/login.log";
+	
+	$date=date("Y/m/d");
+	$time=date("h:i:s");
+	
+	if(!file_exists($dir)){
+		mkdir($dir);
+	}
+	if(!file_exists($file)){
+		$logFile=fopen($file,"w");
+		fwrite($logFile,"Date,Time,User\r\n");
+		fclose();
+	}
+	file_put_contents($file,$date.",".$time.",".$username."\r\n", FILE_APPEND);
+	
+}
+
 function createNewPage($path){
     include("./config/siteVariables.php");
     if(isset($_SESSION["authenticated_tech"])){
