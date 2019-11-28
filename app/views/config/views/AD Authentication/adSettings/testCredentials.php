c929f4679f9719e237a1a9bab0415adfcb2797a1
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 10:19:10 -0500

    Test AD Credentials Implemented

--- app/views/config/views/AD Authentication/adSettings/testCredentials.php
+++ app/views/config/views/AD Authentication/adSettings/testCredentials.php
@@ -12,7 +12,7 @@
         <div>
             <?php //echo  $appConfig["testADCredentials"];
             //echo $_POST["testADCredentialsCheck"];?>
-            <input type="text" name="testADCredentials" hidden/>
+            <input type="text" name="testADCredentials" value="true" hidden/>
             <input type="checkbox"  name="testADCredentialsCheck" value="true" <?php //echo  $appConfig["testADCredentials"];
                    if(isset($appConfig["testADCredentials"])){
                        if($appConfig["testADCredentials"]){
@@ -23,15 +23,25 @@
         </div>
 		<div>
 		</div>
-		<div style = "width:100%;height:100%; text-align:center;display:none;" id="testingADCredentials">
-		<div style="margin-left:auto; margin-right:auto; width:150px;height:150px;" class="loader"></div><br/>
-		Testing Credentials...
-	</div>
-    </div>
-	<script>
-		fillWithHTTPResponse('testingADCredentials','/?grab=/config/testAdCredentials.php');
-	</script>
-	
+		<?php
+if(isset($_POST["testADCredentials"])){
+    
+		if(isset($_POST["testADCredentialsCheck"]) and $_POST['testADCredentialsCheck']==true){
+			   ?>
+				<div style = "width:100%;height:100%; text-align:center;display:none;" id="testingADCredentials">
+				<div style="margin-left:auto; margin-right:auto; width:150px;height:150px;" class="loader"></div><br/>
+				Testing Credentials...
+			</div>
+			</div>
+			<script>
+				fillWithHTTPResponse('testingADCredentials','/?grab=/config/testAdCredentials.php');
+			</script>
+			
+			
+			<?php
+			}
+		}
+		?>
 
 
 
