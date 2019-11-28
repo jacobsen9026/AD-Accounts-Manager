aeed7e9ca7f75a1fd9f47b70fce6398dab4b6610
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 11:20:45 -0500

    implement grab better

--- app/viewController.php
+++ app/viewController.php
@@ -12,7 +12,7 @@ if($_SESSION["authenticated_basic"]!="true" and substr($goto,0,6)!="/error" and 
 }
 //Check if goto variable was set
 //If no goto address was passed show the homepage because the user is logged in
-elseif($goto==null){
+elseif($goto==null and !isset($grab)){
     //Show homepage if goto is blank or missing
     $_SESSION['timeout']=time();
 
@@ -22,11 +22,19 @@ elseif($goto==null){
 }
 //Check the the file goto is calling actually exists
 //If a goto variable file is found show it
-elseif(file_exists("./app/views/".$goto)){
+elseif(file_exists("./app/views/".$goto) and !isset($grab)){
     //Show goto file if it exists
     $_SESSION['timeout']=time();
 
     include("./app/views".$goto);
+}
+elseif (isset($grab)){
+	if ($grab!='' and file_exists("./app/views".$grab)){
+		include ("./app/views".$grab);
+		//exit();
+		
+		
+	}
 }
 
 //Otherwise the file doesn't exist and show an error page
