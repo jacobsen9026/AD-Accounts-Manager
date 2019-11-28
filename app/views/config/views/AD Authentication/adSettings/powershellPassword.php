14d67784b4b6cac46dbda456fcbf7c73183a93f4
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:19:50 -0500

    Create powershellPassword.php

---
+++ app/views/config/views/AD Authentication/adSettings/powershellPassword.php
@@ -0,0 +1,20 @@
+
+<div class="shortSettingsContainer">
+    <div  class="settingsList">
+        <div>
+            <h3>
+                AD Admin Password
+            </h3>
+
+        </div>
+        <div>
+
+
+            <input placeholder="Enter password" type="password" name="powershellPassword" value="<?php echo  $appConfig["powershellPassword"];?>">
+
+        </div>
+
+
+    </div>
+
+</div>
