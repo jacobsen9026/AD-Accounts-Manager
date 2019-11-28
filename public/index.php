92024696f267b2c5123107d97f64848ec83403ce
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:22:57 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,10 @@
+
 <?php
 chdir("../");
 //Show header
 include("./app/header.php");
-
-
 //Show content
 include("./app/viewController.php");
-
-
 //Show footer
 include("./app/footer.php");
-
-//debug("Footer");
-//debug(get_included_files());
-
 ?>
