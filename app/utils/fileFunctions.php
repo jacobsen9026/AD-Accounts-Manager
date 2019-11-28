963e2dc78dc68769f6eb73fada7f9d0d98f2b3bd
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:17:41 -0500

    Save Powershell Password Function Added

--- app/utils/fileFunctions.php
+++ app/utils/fileFunctions.php
@@ -51,6 +51,16 @@ function getFiles($viewPath){
 
 }
 
+function savePowershellCredentials($username, $password){
+	global $appConfig;
+	$passwordFilePath="./app/config/adCred.key";
+	$appConfig["powershellPassword"] = encryptADPassword($password)[0];
+	//echo "teresres";
+	//exit;
+	saveConfig();
+	//file_put_contents($passwordFilePath,
+}
+
 
 
 function auditLogon($username){
