688ac732a712cd436b8bfb8ab22700feda0a1cc8
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:19:02 -0500

    Create testADCredentials.php

---
+++ app/views/config/testADCredentials.php
@@ -0,0 +1,28 @@
+<?php
+if(testADCredentials()){
+?>
+<div>
+        <h3>
+            Credentials Passed
+			</h3>
+    </div>
+    
+<?php
+}else{
+	?>
+	
+	<div>
+        <h3>
+            Credentials Not Working
+			</h3>
+    </div>
+	
+	
+	
+	
+	
+	
+	<?php
+}
+?>
+      
