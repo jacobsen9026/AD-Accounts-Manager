6dea07af83e61f5374e73330b456969a75444b41
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:19:53 -0500

    Create powershellUsername.php

---
+++ app/views/config/views/AD Authentication/adSettings/powershellUsername.php
@@ -0,0 +1,13 @@
+
+<div class="shortSettingsContainer">
+    <div  class="settingsList">
+        <div>
+            <h3>
+                AD Admin Username
+            </h3>
+        </div>
+        <div>
+            <input placeholder="DomainAdmin" type="text" name="powershellUsername" value="<?php echo  $appConfig["powershellUsername"];?>">
+        </div>
+    </div>            
+</div>
