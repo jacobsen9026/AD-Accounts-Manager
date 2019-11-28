122d29d0fd6936514d804c721fe5d636447aeb8f
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:20:22 -0500

    Create downloadOAuth2.php

---
+++ app/views/config/views/GAM Settings/gamSettings/downloadOAuth2.php
@@ -0,0 +1,22 @@
+<?php
+if (file_exists("./lib/gam-64/oauth2.txt")){
+?>
+<div class="settingsContainer">
+    <div  class="settingsList">
+        <div>
+            <h3>OAuth2 Download</h3>
+            <small>Download the current oauth2.txt</small>
+
+
+        </div>
+        <div>
+            <a href="/?download=/lib/gam-64/oauth2.txt" target="_blank"><button type="button">Download</button></a>
+        </div>
+    </div>
+</div>
+
+<?php
+}
+?>
+
+
