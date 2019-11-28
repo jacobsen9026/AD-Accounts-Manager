deb6fade14ab0d6e51820b5a6bd661a89d66d4d1
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:21:52 -0500

    Create getGoogleGroupMembers.php

---
+++ app/views/parents/google-groups/getGoogleGroupMembers.php
@@ -0,0 +1,21 @@
+
+<table class="responseContainer">
+<tr>
+<th>
+Google Group Member List
+</th>
+</tr>
+<tr>
+<td>
+<br/><br/>
+<?php
+printGAParentGroupMembers($_POST["group"]);
+?>
+<br/>
+<a href="/?goto=/parents/google-groups/index.php">
+	<button type="button">Search Another</button>
+</a>
+<br/><br/>
+</td>
+</tr>
+</table>
