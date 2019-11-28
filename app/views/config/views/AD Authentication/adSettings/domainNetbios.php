bdb56c0b9ca0603e32b114360b4fbea9bcdd24be
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-28 00:19:48 -0500

    Create domainNetbios.php

---
+++ app/views/config/views/AD Authentication/adSettings/domainNetbios.php
@@ -0,0 +1,20 @@
+
+<div class="shortSettingsContainer">
+    <div  class="settingsList">
+        <div>
+            <h3>
+                Domain NetBIOS Name
+            </h3>
+
+        </div>
+        <div>
+
+
+            <input title="Enter domain NetBIOS name. Do not enter a FQDN" pattern="^[^\.]*$" placeholder="Enter domain NetBIOS name" type="text" name="domainNetBIOS" value="<?php echo  $appConfig["domainNetBIOS"];?>">
+
+        </div>
+
+
+    </div>
+
+</div>
