401d7e2454e8385eb2c6d734c853d820c94108eb
jacobsen9026 <jacobsen9026@gmail.com>
2019-12-04 22:39:23 -0500

    Update userButton.php

--- app/views/navigation/userButton.php
+++ app/views/navigation/userButton.php
@@ -1,50 +1,57 @@
-<td  class="navigationButton">
-    <?php
-    //if(isset($_SESSION['authenticated_basic'])){
-    //if($_SESSION['authenticated_basic']=="true"){
-    ?>
-    <button id="userButton" onclick="showUserDropdown()">
+<div  class="navigationButton">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+
+            <?php
+            //if(isset($_SESSION['authenticated_basic'])){
+            //if($_SESSION['authenticated_basic']=="true"){
+            ?>
+            <li class="active">
 
         <!--<img src="./img/user_avatar.png"/>-->
-        <div id="loggedInUser">Test User<?php // echo $_SESSION["username"];     ?></div>
-    </button>
+                Test User<?php // echo $_SESSION["username"];          ?>
+            </li>
 
-    <div id="userDropdown" class="subnavigation" >
+            <div id="userDropdown" class="subnavigation" >
 
-        <?php
-        /*
-          if (strpos($_SERVER['REQUEST_URI'], "/?") !== false) {
-          include("./app/views/navigation/user/homeButton.php");
-          }
+                <?php
+                /*
+                  if (strpos($_SERVER['REQUEST_URI'], "/?") !== false) {
+                  include("./app/views/navigation/user/homeButton.php");
+                  }
 
 
-          include("./app/views/navigation/user/themeButton.php");
-          include("./app/views/navigation/user/settingsButton.php");
-          /*
-          if (isset($_SESSION['authenticated_tech'])){
-          if($_SESSION["authenticated_tech"]=="true"){
-          ?>
-          <div id="siteEditorButtonsContainer">
-          <?php
-          include("./app/views/navigation/user/editorButton.php");
-          include("./app/views/navigation/user/editButtons.php");
-          ?>
-          </div>
-          <?php
-          }
-          }
-         */
-        /*
-          include("./app/views/navigation/user/helpButton.php");
-          include("./app/views/navigation/user/helpdeskButton.php");
-          include("./app/views/navigation/user/logoutButton.php");
-         */
-        ?>
-        <span class="spacer"></span>
+                  include("./app/views/navigation/user/themeButton.php");
+                  include("./app/views/navigation/user/settingsButton.php");
+                  /*
+                  if (isset($_SESSION['authenticated_tech'])){
+                  if($_SESSION["authenticated_tech"]=="true"){
+                  ?>
+                  <div id="siteEditorButtonsContainer">
+                  <?php
+                  include("./app/views/navigation/user/editorButton.php");
+                  include("./app/views/navigation/user/editButtons.php");
+                  ?>
+                  </div>
+                  <?php
+                  }
+                  }
+                 */
+                /*
+                  include("./app/views/navigation/user/helpButton.php");
+                  include("./app/views/navigation/user/helpdeskButton.php");
+                  include("./app/views/navigation/user/logoutButton.php");
+                 */
+                ?>
+        </ul>
     </div>
-    <?php
-    // }
+</div>
+<?php
+// }
 //}
-    ?>
+?>
 
-</td>
+</div>
