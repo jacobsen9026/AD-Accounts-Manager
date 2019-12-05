dc9b5223c588d1751a911f4f830bf7f30fe4c2f4
jacobsen9026 <jacobsen9026@gmail.com>
2019-12-04 22:42:03 -0500

    Create CorePrivilege.php

---
+++ system/app/auth/CorePrivilege.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace system\app\auth;
+
+/**
+ * Description of CorePrivilege
+ *
+ * @author cjacobsen
+ */
+class CorePrivilege {
+
+    //put your code here
+    const UNAUTHENTICATED = 0;
+
+}
