1a8ed20ead4b61400b6ae7a1b77d54ec03d2557e
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-07 08:20:46 -0500

    Update debugConfigModal.php

--- app/views/modals/debugConfigModal.php
+++ app/views/modals/debugConfigModal.php
@@ -5,7 +5,7 @@
         <div class="modal-content">
             <div class="modal-header bg-primary text-light">
                 <h4 class="modal-title ">Config</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close text-light" data-dismiss="modal">&times;</button>
 
             </div>
             <div class="modal-body">
