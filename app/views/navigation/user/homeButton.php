352af5b104dc03db55cd16934cf5c69186f8b69c
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-08 23:38:35 -0500

    Clean for rebuild of app

--- app/views/navigation/user/homeButton.php
+++
@@ -1,7 +0,0 @@
-<div>
-			<a href="/">
-				<button>
-				Home
-				</button>
-			</a>
-		</div>
--- public/scripts/CapsLock.js
+++
@@ -1,2 +0,0 @@
-//code.stephenmorley.org
-var CapsLock=function(){var e=/Mac/.test(navigator.platform),b=!1,d=[];window.addEventListener("keypress",function(a){var c=a.charCode;a=a.shiftKey;var f=b;97<=c&&122>=c?b=a:!(65<=c&&90>=c)||a&&e||(b=!a);b!==f&&d.forEach(function(a){return a(b)})});return{isOn:function(){return b},addListener:function(a){d.push(a)}}}();
