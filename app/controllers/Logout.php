bf80287d6888cf54a4662650463936462cd189ab
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-08 23:22:02 -0500

    Create Logout.php

---
+++ app/controllers/Logout.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\controllers;
+
+/**
+ * Description of Logout
+ *
+ * @author cjacobsen
+ */
+class Logout extends Controller {
+
+    public function index() {
+        session_destroy();
+        header("Location: /");
+    }
+
+    //put your code here
+}
