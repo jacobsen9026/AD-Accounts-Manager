93942807eb3403a96ed72dffb8efa8cc42a899f7
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-15 23:39:26 -0500

    Big Improvement

--- app/views/settings/district/schools/nav.php
+++ app/views/settings/district/schools/nav.php
@@ -1,35 +1,83 @@
 
-<div class=" mb-3 rounded row">
-    <div class="col px-1 d-flex align-items-stretch ">
-        <a class="w-100 rounded-0 nav-link btn btn-success" href="/districts/">Back to District</a>
+<script>
+    $(function () {
+        $(".navbar-toggler").on("click", function () {
+            $('.navbar-toggler i').toggleClass("fa-chevron-down fa-chevron-up");
+        });
+    });
+</script>
 
-    </div>
+
+<nav class="navbar navbar-expand-md navbar-dark sticky-top district-nav bg-success shadow-sm w-100">
+    <!-- Brand -->
+    <a class="nav-link text-weight-bold text-light" href="/districts">District Setup</a>
     <?php
-    if (isset($this->schoolID)) {
+    if (!empty($this->district) and isset($this->controller) and $this->controller != 'Districts') {
         ?>
-        <div class="col px-1 d-flex align-items-stretch ">
-            <a class="w-100 rounded-0 nav-link btn btn-success" href="/schools/show/<?php echo $this->districtID; ?>">Back to Schools</a>
+
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar2">
+            <i class="fas fa-chevron-down"></i>
+
+        </button>
+
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar2">
+
+
+
+
+
+
+            <ul class="navbar-nav">
+                <?php
+                if (isset($this->schoolID)) {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/schools/show/<?php echo $this->districtID; ?>">Schools</a>
+                    </li>
+                    <?php
+                }
+                if (isset($this->grades) and isset($this->controller) and $this->controller != 'Schools') {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/schools/edit/<?php echo $this->schoolID; ?>"><?= $this->schoolName; ?></a>
+                    </li>
+                    <?php
+                }
+                if (isset($this->gradeID)) {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/grades/show/<?php echo $this->schoolID; ?>">Grades</a>
+                    </li>
+                    <?php
+                }
+                if (isset($this->teams) and isset($this->controller) and $this->controller != 'Grades') {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/grades/edit/<?php echo $this->gradeID; ?>">Grade <?= $this->grade[\app\database\Schema::GRADES_VALUE]; ?></a>
+                    </li>
+                    <?php
+                }
+                if (isset($this->teamID)) {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/teams/show/<?php echo $this->gradeID; ?>">Teams</a>
+                    </li>
+                    <?php
+                }
+                ?>
+
+
+            </ul>
 
         </div>
-        <?php
-    }
-    if (isset($this->grades)) {
-        ?>
-        <div class="col px-1 d-flex align-items-stretch ">
-            <a class="w-100 rounded-0 nav-link btn btn-success" href="/schools/edit/<?php echo $this->schoolID; ?>">Back to <?php echo $this->schoolName; ?></a>
 
-        </div>
-        <?php
-    }
-    if (isset($this->gradeID)) {
-        ?>
-        <div class="col px-1 d-flex align-items-stretch ">
-            <a class="w-100 rounded-0 nav-link btn btn-success" href="/grades/show/<?php echo $this->schoolID; ?>">Back to Grades</a>
-
-        </div>
         <?php
     }
     ?>
+</nav>
 
 
-</div>
+
