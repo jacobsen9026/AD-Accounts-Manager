4987487e905893317d39483ce850a9b8c55ba24b
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-15 23:38:08 -0500

    Update create.php

--- app/views/settings/district/create.php
+++ app/views/settings/district/create.php
@@ -1,9 +1,9 @@
 <form method="post" name="test" class ="table-hover" action="/districts/create">
-    <div class="container container-lg">
+    <div class="border mt-3 container container-lg py-3">
         <div>
             Create District
         </div>
         <div>Name:<input type="text" name="name"/></div>
-        <button class="btn btn-primary" type="submit">Submit</button>
+        <button class="btn btn-primary " type="submit">Submit</button>
     </div>
 </form>
