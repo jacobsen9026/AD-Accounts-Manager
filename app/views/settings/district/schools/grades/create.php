2435032617cfab06acd4ad22ced33c4cdcdaad4e
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-15 23:38:31 -0500

    Update create.php

--- app/views/settings/district/schools/grades/create.php
+++ app/views/settings/district/schools/grades/create.php
@@ -4,23 +4,25 @@ if (empty($this->grades)) {
     echo $this->view('settings/district/schools/nav');
 }
 ?>
-<form method="post" name="test" class ="table-hover" action="/grades/create/<?php echo $this->schoolID; ?>">
-    <div class="container container-lg">
-        <div>
-            Add Grade
-        </div>
-        <div>Grade Level:
-            <select name="level">
+<div class="p-5">
+    <form method="post" name="test" class ="table-hover" action="/grades/create/<?php echo $this->schoolID; ?>">
+        <div class="container container-lg">
+            <div>
+                Add Grade
+            </div>
+            <div>Grade Level:
+                <select name="<?= \app\database\Schema::GRADES_VALUE ?>">
 
-                <?php
-                foreach (GRADE_CODES as $grade) {
-                    ?>
-                    <option value ="<?php echo $grade; ?>"><?php echo $grade; ?></option>
                     <?php
-                }
-                ?>
-            </select>
+                    foreach (GRADE_CODES as $grade) {
+                        ?>
+                        <option value ="<?php echo $grade; ?>"><?php echo $grade; ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </div>
+            <button class="btn btn-primary" type="submit">Submit</button>
         </div>
-        <button class="btn btn-primary" type="submit">Submit</button>
-    </div>
-</form>
+    </form>
+</div>
