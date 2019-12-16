84fafd0cf984b6dc8c8409890ea9ab973a63fde3
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-15 23:32:10 -0500

    Add Teams Route

--- app/config/Router.php
+++ app/config/Router.php
@@ -49,10 +49,17 @@ class Router extends CoreRouter {
     private function includeCustomRoutes() {
         $this->customRoutes[] = array("install", "index", "home", "index");
 
+        /*
+         * Settings routes
+         */
         $this->customRoutes[] = array("Districts", "*", "settings\Districts", "*");
-
         $this->customRoutes[] = array("Schools", "*", "settings\Schools", "*");
         $this->customRoutes[] = array("Grades", "*", "settings\Grades", "*");
+        $this->customRoutes[] = array("Teams", "*", "settings\Teams", "*");
+
+
+
+
         //var_dump($this->customRoutes);
     }
 
