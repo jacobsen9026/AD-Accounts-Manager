6be2f82f9aa51b97ff331ad1e4603283007ae2e9
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-19 21:50:03 -0500

    Update Lang.php

--- system/Lang.php
+++ system/Lang.php
@@ -18,9 +18,25 @@ abstract class Lang {
     //put your code here
 
     public static function get($name) {
-        $target = '\app\lang\\' . LANG . '\\' . strtoupper(LANG) . 'Common';
+        $requestedLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+        if (self::langExists($requestedLang)) {
+
+            $target = '\app\lang\\' . $requestedLang . '\\' . strtoupper($requestedLang) . 'Common';
+        } else {
+            $target = '\app\lang\\' . DEFAULT_LANG . '\\' . strtoupper(DEFAULT_LANG) . 'Common';
+        }
         //echo $target;
         return $target::get($name);
+    }
+
+    private static function langExists($lang) {
+        if (file_exists(APPPATH . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . $lang . DIRECTORY_SEPARATOR . strtoupper($lang) . 'Common.php')) {
+
+            return true;
+        }
+        app\AppLogger::get()->warning("Language reference for " . $lang . " was not found.");
+        return false;
     }
 
 }
