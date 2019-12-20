0851854a7d76c7c2053eda1d29330dd50ef941e7
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-19 21:42:57 -0500

    Update Grade.php
    Conversion to new Query Class

--- app/models/district/Grade.php
+++ app/models/district/Grade.php
@@ -32,33 +32,76 @@ namespace app\models\district;
  * @author cjacobsen
  */
 use app\database\Schema;
+use app\models\Query;
 
 class Grade {
+
+    const TABLE_NAME = 'Grade';
 
     //put your code here
     public $gradeLevel;
     public $name;
 
     public static function getGrades($schoolID) {
-        return(\system\Database::get()->query('SELECT * From Grades Where ' . Schema::GRADES_SCHOOL_ID . '=' . $schoolID));
+        $query = new Query(self::TABLE_NAME);
+        //var_dump($schoolID);
+        $query->where(Schema::GRADE_SCHOOL_ID, $schoolID)
+                ->leftJoin(Schema::GRADEDEFINITION, Schema::GRADE_GRADE_DEFINITION_ID, Schema::GRADEDEFINITION_VALUE);
+        $result = 'no result';
+        $result = $query->run();
+        //var_dump($result);
+        return $result;
     }
 
     public static function getGrade($gradeID) {
-        return(\system\Database::get()->query('SELECT * From Grades Where ' . Schema::GRADES_ID . '=' . $gradeID)[0]);
+        $query = new Query(self::TABLE_NAME);
+        //var_dump($schoolID);
+        $query->where(Schema::GRADE_ID, $gradeID)
+                ->leftJoin(Schema::GRADEDEFINITION, Schema::GRADE_GRADE_DEFINITION_ID, Schema::GRADEDEFINITION_VALUE);
+        $result = 'no result';
+        $result = $query->run();
+        return $result[0];
+        //return $result;
+        return(\system\Database::get()->query('SELECT * From ' . self::TABLE_NAME . ' Where ' . Schema::GRADE_ID[Schema::COLUMN] . '=' . $gradeID)[0]);
     }
 
     public static function getSchoolID($gradeID) {
-        return(\system\Database::get()->query('SELECT ' . Schema::GRADES_SCHOOL_ID . ' From Grades Where ' . Schema::SCHOOLS_ID . '=' . $gradeID)[0][Schema::GRADES_SCHOOL_ID]);
+        $query = new Query(self::TABLE_NAME, Query::SELECT, Schema::GRADE_SCHOOL_ID[Schema::COLUMN]);
+        $query->where(Schema::GRADE_ID, $gradeID);
+        return $query->run();
     }
 
     public static function createGrade($schoolID, $post) {
         \system\app\AppLogger::get()->debug("Creating new grade for school: " . $schoolID);
-        return \system\Database::get()->query('INSERT INTO Grades (' . Schema::GRADES_VALUE . ',' . Schema::GRADES_SCHOOL_ID . ') VALUES ("' . $post[Schema::GRADES_VALUE] . '", "' . $schoolID . '")');
+
+        $post = \system\Post::getAll();
+        var_dump($post);
+        var_dump(Schema::GRADEDEFINITION_VALUE);
+        $query = new Query(self::TABLE_NAME, Query::INSERT);
+        $query->insert(Schema::GRADE_GRADE_DEFINITION_ID, $post[Schema::GRADEDEFINITION_VALUE[Schema::NAME]])
+                ->insert(Schema::GRADE_SCHOOL_ID, $schoolID);
+        return $query->run();
     }
 
     public static function deleteGrade($gradeID) {
         \system\app\AppLogger::get()->debug("Delete grade id: " . $gradeID);
-        return \system\Database::get()->query('DELETE FROM Grades WHERE ' . Schema::GRADES_ID . '=' . $gradeID);
+        return \system\Database::get()->query('DELETE FROM ' . self::TABLE_NAME . ' WHERE ' . Schema::GRADE_ID[Schema::COLUMN] . '=' . $gradeID);
+    }
+
+    public static function getADSettings($gradeID, $type) {
+        $query = new Query(Schema::ACTIVEDIRECTORY);
+        $query->where(Schema::ACTIVEDIRECTORY_GRADE_ID, $gradeID)
+                ->where(Schema::ACTIVEDIRECTORY_TYPE, $type);
+
+        return $query->run()[0];
+    }
+
+    public static function getGASettings($gradeID, $type) {
+        $query = new Query(Schema::GOOGLEAPPS);
+        $query->where(Schema::GOOGLEAPPS_GRADE_ID, $gradeID)
+                ->where(Schema::GOOGLEAPPS_TYPE, $type);
+
+        return $query->run()[0];
     }
 
 }
