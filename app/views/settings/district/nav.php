e37f2c3254fad257ffbd8966a561598e64cd39a0
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-19 21:45:58 -0500

    Delete nav.php

--- app/views/settings/district/nav.php
+++
@@ -1,9 +0,0 @@
-
-<ul class="nav justify-content-center nav-pills m-3">
-
-
-    <li class="nav-item">
-        <a class="nav-link btn btn-warning" href="/schools/show/<?php echo $this->district["ID"]; ?>">Edit Schools</a>
-    </li>
-
-</ul>
