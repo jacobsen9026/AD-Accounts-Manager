103af3c7dda1e3f3ad63570321aa6a47ddee7286
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-19 21:47:18 -0500

    Update show.php

--- app/views/settings/district/schools/grades/teams/show.php
+++ app/views/settings/district/schools/grades/teams/show.php
@@ -3,19 +3,19 @@
 
 use app\database\Schema;
 
-echo $this->view('settings/district/schools/nav');
+echo $this->view('layouts/setup_navbar');
 if (!empty($this->teams)) {
     ?>
 
     <div class="p-5">
-        <h4>Teams under Grade <?php echo $this->grade[Schema::GRADES_VALUE]; ?></h4>
+        <h4>Teams under Grade <?php // echo $this->grade[Schema::GRADE_VALUE[Schema::COLUMN]];      ?></h4>
 
         <div class="table-responsive-sm">
 
             <table class="mx-auto table table-hover">
                 <thead class="bg-primary text-white">
                     <tr>
-                        <th>Grade Level</th>
+                        <th>Team</th>
                         <th>Edit</th>
                         <th>Remove</th>
                     </tr>
@@ -27,16 +27,16 @@ if (!empty($this->teams)) {
                         ?>
                         <tr>
                             <td>
-                                <?php echo $this->team[Schema::TEAMS_NAME]; ?>
+                                <?php echo $this->team[Schema::TEAM_NAME[Schema::COLUMN]]; ?>
                             </td>
 
 
                             <td>
-                                <a href="/teams/edit/<?php echo $this->team[Schema::TEAMS_ID]; ?>"  class = "btn btn-warning">Edit Team</a>
+                                <a href="/teams/edit/<?php echo $this->team[Schema::TEAM_ID[Schema::COLUMN]]; ?>"  class = "btn btn-warning">Edit Team</a>
                             </td>
                             <td>
                                 <?php echo $this->view('modals/deleteTeam'); ?>
-                                <button type="button" class = "btn btn-danger" data-toggle="modal" data-target="#deleteTeam<?php echo $this->team[Schema::TEAMS_ID]; ?>Modal">Remove Team</button>
+                                <button type="button" class = "btn btn-danger" data-toggle="modal" data-target="#deleteTeam<?php echo $this->team[Schema::TEAM_ID[Schema::COLUMN]]; ?>Modal">Remove Team</button>
 
                             </td>
                         </tr>
