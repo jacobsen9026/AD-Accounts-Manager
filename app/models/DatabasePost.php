6af4f3f1542e86ca17632738e63c95a021bc3d93
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-19 21:42:10 -0500

    Update DatabasePost.php

--- app/models/DatabasePost.php
+++ app/models/DatabasePost.php
@@ -13,6 +13,11 @@ namespace app\models;
  *
  * @author cjacobsen
  */
+use app\database\Schema;
+use system\Database;
+use system\app\AppLogger;
+use app\models\Query;
+
 abstract class DatabasePost {
     //put your code here
 
@@ -21,11 +26,72 @@ abstract class DatabasePost {
      * @param type $tableName
      * @param array $post
      */
-    public static function setPost($tableName, $id, $post) {
-        var_dump($tableName);
+    public static function setPost($primaryTable, $id, $post, $type = null) {
+        $logger = AppLogger::get();
+        $logger->debug('Setting database post');
+        $logger->debug('Primary Table: ' . $primaryTable);
+        $logger->debug('ID: ' . $id);
+        $logger->debug($post);
         foreach ($post as $label => $value) {
-            $query = 'UPDATE ' . $tableName . ' SET ' . $label . ' = "' . $value . '" WHERE ID = ' . $id;
-            \system\Database::get()->query($query);
+
+            $value = self::preProcessValue($label, $value);
+            $column = explode("_", $label, 2)[1];
+            $table = explode("_", $label, 2)[0];
+            if ($column == 'ID') {
+                $logger->info('Skipping ID column');
+                continue;
+            }
+            if ($table != $primaryTable) {
+                //The variable we are saving is different that the table provided to the method
+                // Therefore, the where should match against the given tables ID column
+                //var_dump($table . ' does not equal ' . $primaryTable);
+                //Prepare the Schema Constant Variable
+                $schemaID = strtoupper($table) . '_' . strtoupper($primaryTable) . '_ID';
+
+                $schemaClass = new \ReflectionClass('app\database\Schema');
+                $schema = $schemaClass->getConstant($schemaID);
+                $query = new Query($table, Query::UPDATE, $column);
+                $query->where($schema, $id)
+                        ->set($column, $value);
+                if ($type != null) {
+                    switch ($type) {
+                        case 'Staff':
+                            $query->where(Schema::ACTIVEDIRECTORY_TYPE[Schema::COLUMN], 'Staff');
+
+                            break;
+                        case 'Student':
+
+                            $query->where(Schema::ACTIVEDIRECTORY_TYPE[Schema::COLUMN], 'Student');
+
+                            break;
+
+
+                        default:
+                            break;
+                    }
+                }
+                $query->run();
+                //  $query = 'UPDATE ' . $table . ' SET "' . $column . '" = "' . $value . '" WHERE ' . $schema[Schema::COLUMN] . ' = ' . $id;
+                // Database::get()->query($query);
+            } else {
+
+                $query = 'UPDATE ' . $table . ' SET "' . $column . '" = "' . $value . '" WHERE ID = ' . $id;
+                Database::get()->query($query);
+            }
+        }
+    }
+
+    private static function preProcessValue($label, $value) {
+        //var_dump($label);
+        switch ($label) {
+            case Schema::DISTRICT_AD_NETBIOS[Schema::NAME]:
+                return strtoupper($value);
+
+                break;
+
+            default:
+                return $value;
+                break;
         }
     }
 
