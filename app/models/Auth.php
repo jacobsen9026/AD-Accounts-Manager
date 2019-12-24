96c8c064eb315cc1dbf6175c7167925d95229f18
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-24 14:08:56 -0500

    Update Auth.php
    Laying the groundwork

--- app/models/Auth.php
+++ app/models/Auth.php
@@ -13,11 +13,48 @@ namespace app\models;
  *
  * @author cjacobsen
  */
+use app\models\Query;
+use app\database\Schema;
+use system\app\App;
+
 abstract class Auth {
 
-    //put your code here
+    const TABLE_NAME = 'Auth';
+
+    public static function get() {
+        $query = new Query(self::TABLE_NAME);
+        return $query->run()[0];
+    }
+
+//put your code here
     public static function getSessionTimeout() {
         return 1200;
+    }
+
+    public static function getTechGroup($type) {
+
+    }
+
+    public static function getAdminGroup($type) {
+
+    }
+
+    public static function getPowerGroup($type) {
+
+    }
+
+    public static function getBasicGroup($type) {
+
+    }
+
+    public static function getGroup($type) {
+
+    }
+
+    public static function getLDAPEnabled() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_ENABLED[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
     }
 
 }
