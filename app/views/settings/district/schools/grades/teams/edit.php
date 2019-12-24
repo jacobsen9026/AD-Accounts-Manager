f65f60904773dfd3146ac0de501191eb91c1ce5a
Chris Jacobsen Work <cjacobsen@work.com>
2019-12-24 14:07:26 -0500

    Update edit.php

--- app/views/settings/district/schools/grades/teams/edit.php
+++ app/views/settings/district/schools/grades/teams/edit.php
@@ -3,6 +3,9 @@
 <?php
 
 use app\database\Schema;
+use system\app\Form;
+use app\models\district\ActiveDirectory;
+use app\models\district\GoogleApps;
 
 echo $this->view('layouts/setup_navbar');
 ?>
@@ -12,37 +15,79 @@ echo $this->view('layouts/setup_navbar');
     ?>
     <h4>Editing Team <?php echo $this->team[Schema::TEAM_NAME[Schema::COLUMN]]; ?> at <?= $this->school[Schema::SCHOOL_NAME[Schema::COLUMN]] ?></h4>
 
+    <?php
+//var_dump($this->district);
+    $form = new Form('/teams/edit/' . $this->team[Schema::GRADE_ID[Schema::COLUMN]]);
+    $form->buildTextInput('Parent Email Group', Schema::GRADE_PARENT_EMAIL_GROUP[Schema::NAME], $this->team[Schema::SCHOOL_PARENT_EMAIL_GROUP[Schema::COLUMN]])
+            ->appendInput('@' . $this->district[Schema::DISTRICT_GA_FQDN[Schema::COLUMN]])
+            ->addToRow(3);
 
-    <form method = "post" action = "/teams/edit/<?php echo $this->team[Schema::TEAM_ID[Schema::COLUMN]]; ?>">
-        <?php
-//formTextInput('Name', Schema::TEAMS_NAME, $this->team[Schema::TEAMS_NAME]);
-        ?>
 
-        <div class="form-group mt-5">
-            <?php
-            // formTextInput('Staff Google Apps OU', Schema::TEAMS_STAFF_GA_OU, $this->team[Schema::TEAMS_STAFF_GA_OU]);
-            // formTextInput('Staff Active Directory OU', Schema::TEAMS_STAFF_AD_OU, $this->team[Schema::TEAMS_STAFF_AD_OU]);
-            // formTextInput('Staff Active Directory Group', Schema::TEAMS_STAFF_AD_GROUP, $this->team[Schema::TEAMS_STAFF_AD_GROUP]);
-            // formTextInput('Staff Google Apps Group', Schema::TEAMS_STAFF_GA_GROUP, $this->team[Schema::TEAMS_STAFF_GA_GROUP]);
-            ?>
+    $form->buildUpdateButton()->addToRow(100);
+
+    echo $form->getFormHTML();
+    ?>
+    <button class="my-1 btn btn-success collapse show ad_ga_settings"  type="button" data-toggle="collapse" data-target=".ad_ga_settings" aria-expanded="false" aria-controls="collapseExample">
+        Show More Settings
+    </button>
+    <div class="collapse ad_ga_settings" id="ad_ga_settings">
+
+        <nav class=" nav-fill nav-justified">
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <a class="rounded-0 shadow bg-primary text-light nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-staff-ad" role="tab" aria-controls="nav-home" aria-selected="true">Staff Active Directory</a>
+                <a class="rounded-0 shadow bg-primary text-light nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-staff-ga" role="tab" aria-controls="nav-profile" aria-selected="false">Staff Google Apps</a>
+                <a class="rounded-0 shadow bg-primary text-light nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-student-ad" role="tab" aria-controls="nav-home" aria-selected="true">Student Active Directory</a>
+                <a class="rounded-0 shadow bg-primary text-light nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-student-ga" role="tab" aria-controls="nav-profile" aria-selected="false">Student Google Apps</a>
+
+            </div>
+        </nav>
+        <div class="shadow bg-light tab-content py-3 px-md-3" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-staff-ad" role="tabpanel" aria-labelledby="nav-staff-tab">
+                <?php
+                var_dump(ActiveDirectory::getField(Schema::GRADE, $this->teamID, Schema::ACTIVEDIRECTORY_OU, 'Staff'));
+                $staffADForm = new Form('/teams/editStaff/' . $this->team[Schema::GRADE_ID[Schema::COLUMN]]);
+                $staffADForm->generateADForm($this->teamID, $this->staffADSettings, Schema::TEAM)
+                        ->buildUpdateButton()->addToRow(100);
+                echo $staffADForm->getFormHTML();
+                ?>
+            </div>
+            <div class="tab-pane fade" id="nav-staff-ga" role="tabpanel" aria-labelledby="nav-student-tab">
+                <?php
+                $staffGAForm = new Form('/teams/editStaff/' . $this->team[Schema::GRADE_ID[Schema::COLUMN]]);
+                $staffGAForm->generateGAForm($this->teamID, $this->staffGASettings, Schema::TEAM)
+                        ->buildUpdateButton('Update GA')
+                        ->addToNewRow();
+
+                echo $staffGAForm->getFormHTML();
+                ?>
+            </div>
+            <div class="tab-pane fade" id="nav-student-ad" role="tabpanel" aria-labelledby="nav-staff-tab">
+                <?php
+                $studentADForm = new Form('/teams/editStudents/' . $this->team[Schema::GRADE_ID[Schema::COLUMN]]);
+                $studentADForm->generateADForm($this->teamID, $this->studentADSettings, Schema::TEAM, 'Student')
+                        ->buildUpdateButton('Update AD')
+                        ->addToNewRow();
+
+
+                echo $studentADForm->getFormHTML();
+                ?>
+            </div>
+            <div class="tab-pane fade" id="nav-student-ga" role="tabpanel" aria-labelledby="nav-student-tab">
+                <?php
+                $studentGAForm = new Form('/teams/editStudents/' . $this->team[Schema::GRADE_ID[Schema::COLUMN]]);
+                $studentGAForm->generateGAForm($this->teamID, $this->studentGASettings, Schema::TEAM, 'Student')
+                        ->buildUpdateButton('Update GA')
+                        ->addToNewRow();
+
+
+                echo $studentGAForm->getFormHTML();
+                ?>
+            </div>
         </div>
 
 
-        <div class="form-group mt-5">
+    </div>
 
-            <?php
-            //formTextInput('Student Google Apps OU', Schema::TEAMS_STUDENT_GA_OU, $this->team[Schema::TEAMS_STUDENT_GA_OU]);
-            //formTextInput('Student Active Directory OU', Schema::TEAMS_STUDENT_AD_OU, $this->team[Schema::TEAMS_STUDENT_AD_OU]);
-            // formTextInput('Student Google Group', Schema::TEAMS_STUDENT_GA_GROUP, $this->team[Schema::TEAMS_STUDENT_GA_GROUP]);
-            //  formTextInput('Student Active Directory Group', Schema::TEAMS_STUDENT_AD_GROUP, $this->team[Schema::TEAMS_STUDENT_AD_GROUP]);
-            //formBinaryInput('Force Password Changes', Schema::TEAMS_FORCE_STUDENT_PASSWORD_CHANGE, $this->team[Schema::TEAMS_FORCE_STUDENT_PASSWORD_CHANGE]);
-            ?>
-        </div>
-
-
-
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
 </div>
 
 
