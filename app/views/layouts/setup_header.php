b8ace0242566cf2277d92edef7c87dcb78bc3a0c
Chris Jacobsen Work <cjacobsen@work.com>
2020-01-01 11:03:06 -0500

    Update setup_header.php
    Style adjustment

--- app/views/layouts/setup_header.php
+++ app/views/layouts/setup_header.php
@@ -43,7 +43,7 @@
     <div class="container-fluid centered px-0 px-md-5">
 
 
-        <div class='centered text-center text_centered container-fluid  px-0 pb-3  mt-md-8 mb-5 mx-auto shadow-lg mt-5 pt-2 pt-md-0'>
+        <div class='centered text-center text_centered container-fluid  px-0   mt-md-8 mb-5 mx-auto shadow-lg mt-5 pt-2 pt-md-0'>
 
 
 
