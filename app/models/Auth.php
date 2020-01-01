2fca998810aae502974518d29c815ff6a28d58f3
Chris Jacobsen Work <cjacobsen@work.com>
2020-01-01 10:58:58 -0500

    Update Auth.php
    Implement gets for auth groups

--- app/models/Auth.php
+++ app/models/Auth.php
@@ -31,20 +31,52 @@ abstract class Auth {
         return 1200;
     }
 
-    public static function getTechGroup($type) {
-
+    public static function getTechGAGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_TECH_GA_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
     }
 
-    public static function getAdminGroup($type) {
-
+    public static function getAdminGAGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_ADMIN_GA_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
     }
 
-    public static function getPowerGroup($type) {
-
+    public static function getPowerGAGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_POWER_GA_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
     }
 
-    public static function getBasicGroup($type) {
+    public static function getBasicGAGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_BASIC_GA_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
 
+    public static function getTechADGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_TECH_AD_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getAdminADGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_ADMIN_AD_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getPowerADGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_POWER_AD_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getBasicADGroup() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_BASIC_AD_GROUP[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
     }
 
     public static function getGroup($type) {
@@ -53,6 +85,42 @@ abstract class Auth {
 
     public static function getLDAPEnabled() {
         $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_ENABLED[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getLDAPUseSSL() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_USE_SSL[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getLDAPServer() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_SERVER[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getLDAP_FQDN() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_FQDN[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getLDAP_Port() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_PORT[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getLDAPUsername() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_USERNAME[Schema::COLUMN]]);
+        $query->where(Schema::AUTH_APP_ID, App::getID());
+        return $query->run();
+    }
+
+    public static function getLDAPPassword() {
+        $query = new Query(self::TABLE_NAME, Query::SELECT, [Schema::AUTH_LDAP_PASSWORD[Schema::COLUMN]]);
         $query->where(Schema::AUTH_APP_ID, App::getID());
         return $query->run();
     }
