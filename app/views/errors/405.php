907b41018ba5be1343ca894b8cf0372c9f3b54bf
Chris Jacobsen Work <cjacobsen@work.com>
2020-01-01 11:02:13 -0500

    Update 405.php
    Add controller to 405 error message, should be removed for production

--- app/views/errors/405.php
+++ app/views/errors/405.php
@@ -11,6 +11,6 @@
     <div>
 
         The page you requested was unable to fully complete processing due to a lack of implementation for a requested function.<br/>
-
+        <?= $this->router->controller . "->" . $this->router->method; ?>
     </div>
 </div>
