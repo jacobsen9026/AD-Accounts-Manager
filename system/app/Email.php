da3eb0f4368bb37dc124fa66281e986457736e5e
Chris Jacobsen Work <cjacobsen@work.com>
2020-01-07 10:48:27 -0500

    Create Email.php

---
+++ system/app/Email.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace system\app;
+
+/**
+ * Description of Email
+ *
+ * @author cjacobsen
+ */
+class Email {
+
+    //put your code here
+
+    public function send($to, $cc, $bcc, $subject, $body, $replyTo) {
+        
+    }
+
+    public function testConnection() {
+        return false;
+    }
+
+}
