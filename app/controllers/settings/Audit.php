6f0ec29b523950485d36de236cd96725a2bec874
jacobsen9026 <jacobsen9026@gmail.com>
2020-10-29 15:30:41 -0400

    Add audit viewing module

---
+++ app/controllers/settings/Audit.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2020 cjacobsen.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace App\Controllers\Settings;
+
+/**
+ * Description of Authentication
+ *
+ * @author cjacobsen
+ */
+
+use DateInterval;
+use DateTime;
+use System\Post;
+use App\Models\User\PrivilegeLevel;
+use App\Models\Database\PrivilegeLevelDatabase;
+use App\Controllers\Controller;
+use App\Models\Database\EmailDatabase;
+
+class Audit extends Controller
+{
+
+    public function index()
+    {
+
+        $this->tab = 'audit';
+
+        $this->fromTime = new DateTime();
+        $this->toTime = new DateTime();
+        $this->fromTime->sub(new DateInterval('P1D'));
+        $this->fromTime=$this->fromTime->format('Y-m-d')."T".$this->fromTime->format('H:i');
+        $this->toTime=$this->toTime->format('Y-m-d')."T".$this->toTime->format('H:i');
+
+        $this->audit = \App\Models\Database\AuditDatabase::getLast24Hrs();
+
+        return $this->view('settings/audit');
+    }
+
+
+    public function indexPost()
+    {
+
+        $post = \system\Post::getAll();
+        $this->fromTime = Post::get("fromTime");
+        $this->toTime = Post::get("toTime");
+        $fromTime = new \DateTime($this->fromTime);
+        $toTime = new \DateTime($this->toTime);
+        $this->tab = 'audit';
+        $this->audit = \App\Models\Database\AuditDatabase::getBetween($fromTime->getTimestamp(),$toTime->getTimestamp());
+
+
+        return $this->view('settings/audit');
+    }
+
+}
---
+++ app/views/settings/audit.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2019 cjacobsen.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+use App\App\App;
+use App\App\AppUpdater;
+use App\Models\View\Javascript;
+use App\Models\View\Modal;
+use System\App\Forms\Form;
+use System\App\Forms\FormButton;
+use System\App\Forms\FormSlider;
+use System\App\Forms\FormText;
+use System\Core;
+
+/**
+ *  Form Button that opens a modal with a form in it
+ */
+//$audit = \App\Models\Database\AuditDatabase::getLast24Hrs();
+
+?>
+<h5>
+    Audit Log
+</h5>
+<small>Times are in UTC</small>
+<?php
+$form = new Form('/settings/audit','audit');
+
+$fromTime = new \System\App\Forms\FormDate('From Time',null,'fromTime',$this->fromTime);
+$toTime = new \System\App\Forms\FormDate('To Time',null,'toTime',$this->toTime);
+
+$fromTime->auto();
+$toTime->auto();
+
+
+$submit = new FormButton('Search','small');
+$submit->setType('submit');
+
+$form->addElementToNewRow($fromTime)
+->addElementToCurrentRow($toTime)
+->addElementToCurrentRow($submit);
+echo $form->print();
+?>
+<div class="w-100">
+    <div class="row">
+    <strong class="col-3">
+        Timestamp
+    </strong>
+    <strong class="col-1">
+        Username
+    </strong>
+    <strong class="col-1">
+        IP
+    </strong>
+    <strong class="col-2">
+        Action
+    </strong>
+    <strong class="col-5">
+        Description
+    </strong>
+    </div>
+    <?php
+    if(is_array($this->audit) && count($this->audit)>0){
+        foreach($this->audit as $entry){
+            $timestamp = new DateTime();
+            $timestamp->setTimestamp($entry['Timestamp']);
+            ?>
+            <div class="row">
+                <div class="col-3">
+    <?=$timestamp->format('Y-m-d H:i:s')?>
+                </div>
+                <div class="col-1">
+                    <?=$entry['Username']?>
+
+                </div>
+                <div class="col-1">
+                    <?=$entry['IP']?>
+
+                </div>
+                <div class="col-2">
+                    <?=$entry['Action']?>
+
+                </div>
+                <div class="col-5">
+                    <?=$entry['Description']?>
+
+                </div>
+            </div>
+
+
+                <?php
+        }
+    }else{
+        echo "No results found.";
+    }
+
+
+    ?>
+
+</div>
+<?php
+
