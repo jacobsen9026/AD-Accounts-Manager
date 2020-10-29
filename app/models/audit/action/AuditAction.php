aee4b42f4b12b75b255c36d45404f5c498f64fa3
jacobsen9026 <jacobsen9026@gmail.com>
2020-10-29 15:30:59 -0400

    Fix for audit when no user is logged in.

--- app/models/audit/action/AuditAction.php
+++ app/models/audit/action/AuditAction.php
@@ -18,8 +18,33 @@ class AuditAction
     public function __construct()
     {
         /* @var $user \App\Models\User\User */
-        $this->$user = APPCLASS::get()->user;
+        if($this->user==null) {
+            if(class_exists(APPCLASS)) {
+                $appClass=APPCLASS;
+                $app = $appClass::get();
+                if(isset($app->user ) && $app->user !=null) {
+                    $this->user = $app->user;
+                }
+            }
+        }
     }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user): void
+    {
+        $this->user = $user;
+    }
+
 
 
     /**
