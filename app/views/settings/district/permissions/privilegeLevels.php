d92964b55cc5e01f9d968a15270cdae3c954a462
jacobsen9026 <jacobsen9026@gmail.com>
2020-10-29 15:35:15 -0400

    Collapsable instructions

--- app/views/settings/district/permissions/privilegeLevels.php
+++ app/views/settings/district/permissions/privilegeLevels.php
@@ -30,26 +30,31 @@ use App\Models\View\PermissionMapPrinter;
 ?>
 <h4 class="mb-3">
     Manage Privilege Levels
+
+        <div class="text-secondary clickable far fa-question-circle fa-xs" data-toggle="collapse" data-target="#privilegeLevelCollapse"></div>
+
 </h4>
 
-<div class="row p-2">
-    <div class="col">
-        Privilege levels can be assigned at the District or OU level with propagation. So a group with
-        permission for a School OU will have that same permission for all sub-OU's, unless explicitly set at a lower
-        level.
+<div class="collapse" id="privilegeLevelCollapse">
+    <div class="row p-2">
+        <div class="col">
+            Privilege levels can be assigned at the District or OU level with propagation. So a group with
+            permission for a School OU will have that same permission for all sub-OU's, unless explicitly set at a lower
+            level.
+
+        </div>
+        <div class="col">
+            Permissions are different for users or groups, but they both are successive levels of access that inherit the
+            previous level.
+            A group with Change permission can also Read, but can not Add or Delete (for groups).
+        </div>
     </div>
-</div>
-<div class="row p-2 m-3">
-    <div class="col">
-        Permissions are different for users or groups, but they both are successive levels of access that inherit the
-        previous level.
-        A group with Change permission can also Read, but can not Add or Delete (for groups).
-    </div>
-</div>
-<div class="row p-2 m-3">
-    <div class="col">
-        Privilege Levels with Super Admin enabled have permission to the whole directory as well as settings and setup
-        pages.
+
+    <div class="row p-2 m-3">
+        <div class="col">
+            Privilege Levels with Super Admin enabled have permission to the whole directory as well as settings and setup
+            pages.
+        </div>
     </div>
 </div>
 <div class="row">
