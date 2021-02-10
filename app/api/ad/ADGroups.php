c349f367b2b66929f89c1eff88f3d3eea118c0ee
Chris Jacobsen Work <cjacobsen@work.com>
2021-02-10 15:26:57 -0500

    Added getChildren method, class renaming

--- app/api/ad/ADGroups.php
+++ app/api/ad/ADGroups.php
@@ -5,10 +5,12 @@ namespace App\Api\Ad;
 
 
 use Adldap\Models\Group;
-use App\Models\Database\DistrictDatabase;
+use App\App\App;
+use App\Models\Database\DomainDatabase;
 use App\Models\User\PermissionHandler;
 use App\Models\User\PermissionLevel;
 use System\App\AppException;
+use System\App\AppLogger;
 use System\App\LDAPLogger;
 
 class ADGroups extends ADApi
@@ -22,7 +24,7 @@ class ADGroups extends ADApi
     public static function getGroup($groupName, $baseDN = null)
     {
         if (is_null($baseDN) or $baseDN === '') {
-            $baseDN = self::getOUFromDN(DistrictDatabase::getAD_BaseDN());
+            $baseDN = self::getOUFromDN(DomainDatabase::getAD_BaseDN());
         }
 
         LDAPLogger::get()->info("Getting " . $groupName . " from Active Directory");
@@ -79,5 +81,50 @@ class ADGroups extends ADApi
     public static function createGroup(\App\Models\District\AddDistrictGroup $param)
     {
 
+    }
+
+    public static function getChildren(?string $distinguishedName)
+    {
+        //$filter = '(&(sAMAccountName=*)(memberOf='+$distinguishedName+'))';
+        $groups = ADConnection::getConnectionProvider()->search()
+            ->groups()
+            ->where('memberOf', '=', $distinguishedName)
+            ->get();
+        return $groups;
+    }
+
+    public static function getGroupByDN($dn, $baseDN = null)
+    {
+        if (is_null($baseDN) or $baseDN === '') {
+            $baseDN = self::getOUFromDN(DomainDatabase::getAD_BaseDN());
+        }
+
+        LDAPLogger::get()->info("Getting " . $dn . " from Active Directory");
+        if ($dn instanceof Group) {
+            return $dn;
+        }
+        $conn = ADConnection::getConnectionProvider();
+        $group = $conn
+            ->search()
+            ->groups()
+            ->in($baseDN)
+            ->where("distinguishedName", '=', $dn)
+            ->limit(1)
+            ->get()[0];
+
+
+        LDAPLogger::get()->debug($group);
+        if ($group == null || !$group->exists) {
+            $group = $conn->search()->findByDn($dn);
+            LDAPLogger::get()->debug($group);
+        }
+        if ($group == null || !$group->exists) {
+            throw new AppException('That group was not found.', AppException::GROUP_NOT_FOUND);
+        }
+        if (!PermissionHandler::hasPermission(self::getOUFromDN($group->getDistinguishedName()), PermissionLevel::GROUPS, PermissionLevel::GROUP_READ)) {
+            throw new AppException('That group was not found.', AppException::FAIL_GROUP_READ_PERM);
+
+        }
+        return $group;
     }
 }
