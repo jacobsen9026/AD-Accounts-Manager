1ade7e024440e7cf042d275538a3c645925c3f1d
Chris Jacobsen Work <cjacobsen@work.com>
2021-02-11 10:46:27 -0500

    Remove obsolete custom routes

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -13,9 +13,11 @@ $this->customRoutes[] = ["install", "index", "home", "index"];
 /*
  * Settings routes
  */
+/*
 $this->customRoutes[] = ["Districts", "*", "settings\Districts", "*"];
 $this->customRoutes[] = ["Schools", "*", "settings\Schools", "*"];
 $this->customRoutes[] = ["Grades", "*", "settings\Grades", "*"];
 $this->customRoutes[] = ["Departments", "*", "settings\Departments", "*"];
 $this->customRoutes[] = ["Teams", "*", "settings\Teams", "*"];
+*/
 
