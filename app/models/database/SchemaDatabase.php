dfa8d6e37724c62c30d13e1ab6f9f8f2df623826
Chris Jacobsen Work <cjacobsen@work.com>
2021-02-11 10:46:48 -0500

    Add isSchemaUpdateAvailable method

--- app/models/database/SchemaDatabase.php
+++ app/models/database/SchemaDatabase.php
@@ -4,6 +4,8 @@
 namespace App\Models\Database;
 
 
+use System\File;
+
 class SchemaDatabase extends DatabaseModel
 {
     const TABLE_NAME = "Schema";
@@ -14,5 +16,33 @@ class SchemaDatabase extends DatabaseModel
     public static function getSchemaVersion()
     {
         return self::getDatabaseValue('Schema_Version', 1);
+    }
+
+    public static function isSchemaUpdateAvailable(){
+        if(self::getNextSchemaVersion()!==null){
+            return true;
+        }
+        return false;
+    }
+
+    public static function getNextSchemaVersion()
+    {
+        $currentVersion = self::getSchemaVersion();
+        $currentVersionNumber = explode('.', $currentVersion);
+        foreach(File::getFiles(APPPATH.DIRECTORY_SEPARATOR."database".DIRECTORY_SEPARATOR."updates")as $schemaUpdateFile){
+            $availableVersion = str_replace("v","",pathinfo($schemaUpdateFile)['filename']);
+            $availableVersionNumber = explode('.', $availableVersion);
+
+
+                if($availableVersionNumber[0]>$currentVersionNumber[0]){
+                    $nextVersion = $availableVersion;
+                }elseif ($availableVersionNumber[1]>$currentVersionNumber[1]){
+                    $nextVersion = $availableVersion;
+                }elseif ($availableVersionNumber[2]>$currentVersionNumber[2]){
+                    $nextVersion = $availableVersion;
+                }
+
+        }
+        return $nextVersion;
     }
 }
