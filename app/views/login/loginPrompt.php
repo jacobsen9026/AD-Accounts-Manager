a8236522eb8df01951f8272a4a3885906838288b
Chris Jacobsen Work <cjacobsen@work.com>
2021-02-11 10:39:34 -0500

    Code cleanup Move username and password labels to placeholders

--- app/views/login/loginPrompt.php
+++ app/views/login/loginPrompt.php
@@ -1,26 +1,31 @@
 <?php
 
+use App\Forms\FormText;
+
+use App\Models\View\Toast;
 use System\App\Forms\Form;
-use System\App\Forms\FormText;
 use System\App\Forms\FormButton;
 
 use System\Lang;
+use System\Request;
 
 if (isset($params['toast'])) {
     echo $params['toast'];
 
 }
-if (\System\Request::get()->serverName == 'demo.adam-app.gq') {
+if (Request::get()->serverName == 'demo.adam-app.gq') {
     $toastBody = "<strong>To login use</strong><br>demo<br>demo<br><br>";
-    $demoToast = new \App\Models\View\Toast('Demo Account', $toastBody, 10000);
+    $demoToast = new Toast('Demo Account', $toastBody, 10000);
     $demoToast->closable();
     echo $demoToast->printToast();
 }
 $form = new Form();
-$username = new FormText(Lang::get("Username"), '', 'username');
-$username->autofocus();
-$password = new FormText(Lang::get("Password"), '', 'password');
-$password->isPassword();
+$username = new FormText('', '', 'username');
+$username->autofocus()
+    ->setPlaceholder(Lang::get("Username"));
+$password = new FormText('', '', 'password');
+$password->isPassword()
+    ->setPlaceholder(Lang::get("Password"));
 $loginButton = new FormButton(Lang::get("Login"));
 $loginButton->setTheme('secondary');
 $form->addElementToNewRow($username)
