beab1171e78f2d7d72ad266e2f65b53ce9e853f6
Chris Jacobsen Work <cjacobsen@work.com>
2021-02-12 09:44:04 -0500

    Remove unnecessary Div layer

--- app/views/layouts/toast.php
+++ app/views/layouts/toast.php
@@ -1,34 +1,34 @@
-<div class="<?= $params['toastClasses'] ?>" style="z-index:1030">
-    <div id="<?= $params['id'] ?>" class="toast mx-auto" role="alert" aria-live="assertive" aria-atomic="true"
-         data-autohide="true">
-        <div class="toast-header">
-            <?= $params['image'] ?>
+<div id="<?= $params['id'] ?>" class="toast mx-auto <?= $params['toastClasses'] ?>" role="alert"
+     aria-live="assertive" aria-atomic="true"
+     data-autohide="true"
+     style="z-index:1030">
+    <div class="toast-header">
+        <?= $params['image'] ?>
 
-            <strong class="ml-1 mr-auto">
-                <?= $params['header'] ?></strong>
-            <?php
-            if ($params['timeout'] === 0 || $params['closable']) {
-                ?>
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <?php
-            }
-            ?>
-        </div>
+        <strong class="ml-1 mr-auto">
+            <?= $params['header'] ?></strong>
         <?php
-        if ($params['body'] != '') {
+        if ($params['timeout'] === 0 || $params['closable']) {
             ?>
-            <div class="toast-body">
-                <?= $params['body'] ?>
-            </div>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
             <?php
         }
         ?>
-        <script>
-
-            $(".toast").toast({'animation': true, 'autohide': true, 'delay': <?=$params['timeout']?>});
-            $(".toast").toast("show");
-        </script>
     </div>
+    <?php
+    if ($params['body'] != '') {
+        ?>
+        <div class="toast-body">
+            <?= $params['body'] ?>
+        </div>
+        <?php
+    }
+    ?>
+    <script>
+
+        $(".toast").toast({'animation': true, 'autohide': true, 'delay': <?=$params['timeout']?>});
+        $(".toast").toast("show");
+    </script>
 </div>
