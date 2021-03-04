9f981a514a555b7505ba49fe64b9712137715b97
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:54:30 -0500

    Code cleanup

--- app/models/database/SchemaDatabase.php
+++ app/models/database/SchemaDatabase.php
@@ -10,16 +10,9 @@ class SchemaDatabase extends DatabaseModel
 {
     const TABLE_NAME = "Schema";
 
-    /**
-     * @return mixed
-     */
-    public static function getSchemaVersion()
+    public static function isSchemaUpdateAvailable()
     {
-        return self::getDatabaseValue('Schema_Version', 1);
-    }
-
-    public static function isSchemaUpdateAvailable(){
-        if(self::getNextSchemaVersion()!==null){
+        if (self::getNextSchemaVersion() !== null) {
             return true;
         }
         return false;
@@ -29,20 +22,42 @@ class SchemaDatabase extends DatabaseModel
     {
         $currentVersion = self::getSchemaVersion();
         $currentVersionNumber = explode('.', $currentVersion);
-        foreach(File::getFiles(APPPATH.DIRECTORY_SEPARATOR."database".DIRECTORY_SEPARATOR."updates")as $schemaUpdateFile){
-            $availableVersion = str_replace("v","",pathinfo($schemaUpdateFile)['filename']);
+        $nextVersion = $currentVersion;
+        $availableVersions = self::getAvailableVersions();
+        foreach ($availableVersions as $availableVersion) {
             $availableVersionNumber = explode('.', $availableVersion);
 
 
-                if($availableVersionNumber[0]>$currentVersionNumber[0]){
-                    $nextVersion = $availableVersion;
-                }elseif ($availableVersionNumber[1]>$currentVersionNumber[1]){
-                    $nextVersion = $availableVersion;
-                }elseif ($availableVersionNumber[2]>$currentVersionNumber[2]){
-                    $nextVersion = $availableVersion;
-                }
+            if ($availableVersionNumber[0] > $currentVersionNumber[0]) {
+                $nextVersion = $availableVersion;
+            } elseif ($availableVersionNumber[1] > $currentVersionNumber[1]) {
+                $nextVersion = $availableVersion;
+            } elseif ($availableVersionNumber[2] > $currentVersionNumber[2]) {
+                $nextVersion = $availableVersion;
+            }
 
         }
         return $nextVersion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getSchemaVersion()
+    {
+        $schemaVersion = self::getDatabaseValue('Schema_Version', 1);
+        if ($schemaVersion === false) {
+            self::insertRow();
+            self::getSchemaVersion();
+        }
+        return $schemaVersion;
+    }
+
+    public static function getAvailableVersions()
+    {
+        foreach (File::getFiles(APPPATH . DIRECTORY_SEPARATOR . "database" . DIRECTORY_SEPARATOR . "updates") as $schemaUpdateFile) {
+            $availableVersions[] = str_replace("v", "", pathinfo($schemaUpdateFile)['filename']);
+        }
+        return $availableVersions;
     }
 }
