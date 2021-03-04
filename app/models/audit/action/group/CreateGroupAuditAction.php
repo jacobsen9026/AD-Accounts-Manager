66d8fd4e144382b37d5ff6bfd742c013ed571aff
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:08:31 -0500

    New audit actions

---
+++ app/models/audit/action/group/CreateGroupAuditAction.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Models\Audit\Action\Group;
+
+
+use App\Models\Audit\Action\AuditAction;
+use App\Models\District\DomainGroup;
+
+class CreateGroupAuditAction extends AuditAction
+{
+
+    /**
+     * DeleteGroupAuditAction constructor.
+     *
+     * @param DomainGroup $group
+     */
+    public function __construct(DomainGroup $group)
+    {
+
+        $this->setType('Group_Create');
+        $this->setDescription('Created group: ' . $group->activeDirectory->getName() . ' at ' . $group->getOU());
+
+    }
+}
---
+++ app/models/audit/action/user/CreateUserAuditAction.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Models\Audit\Action\User;
+
+use App\Models\Audit\Action\AuditAction;
+use App\Models\District\DomainUser;
+
+class CreateUserAuditAction extends AuditAction
+{
+
+    /**
+     * CreateUserAuditAction constructor.
+     *
+     * @param DomainUser $user
+     */
+    public function __construct(DomainUser $user)
+    {
+
+        $this->setType('User_Created');
+        $this->setDescription('Created: ' . $user->getUsername() . ' at ' . $user->getOU());
+
+    }
+}
