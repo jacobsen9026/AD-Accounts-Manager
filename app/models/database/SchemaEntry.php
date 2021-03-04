70bfb9a560cc5dcaff8bc44f0971a574ec62b0b8
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:54:53 -0500

    New schemaEntry class for adding columns

---
+++ app/models/database/SchemaEntry.php
@@ -0,0 +1,113 @@
+<?php
+
+
+namespace App\Models\Database;
+
+
+class SchemaEntry
+{
+    protected string $columnName;
+    protected string $dataType;
+    protected $defaultValue;
+    protected bool $notNull;
+    protected bool $primaryKey;
+
+    /**
+     * SchemaEntry constructor.
+     * @param string $columnName
+     * @param string $dataType
+     * @param $defaultValue
+     * @param bool $notNull
+     * @param bool $primaryKey
+     */
+    public function __construct(string $columnName, string $dataType, $defaultValue, bool $notNull, bool $primaryKey)
+    {
+        $this->columnName = $columnName;
+        $this->dataType = $dataType;
+        $this->defaultValue = $defaultValue;
+        $this->notNull = $notNull;
+        $this->primaryKey = $primaryKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColumnName(): string
+    {
+        return $this->columnName;
+    }
+
+    /**
+     * @param string $columnName
+     */
+    public function setColumnName(string $columnName): void
+    {
+        $this->columnName = $columnName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDataType(): string
+    {
+        return $this->dataType;
+    }
+
+    /**
+     * @param string $dataType
+     */
+    public function setDataType(string $dataType): void
+    {
+        $this->dataType = $dataType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefaultValue()
+    {
+        return $this->defaultValue;
+    }
+
+    /**
+     * @param mixed $defaultValue
+     */
+    public function setDefaultValue($defaultValue): void
+    {
+        $this->defaultValue = $defaultValue;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNotNull(): bool
+    {
+        return $this->notNull;
+    }
+
+    /**
+     * @param bool $notNull
+     */
+    public function setNotNull(bool $notNull): void
+    {
+        $this->notNull = $notNull;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPrimaryKey(): bool
+    {
+        return $this->primaryKey;
+    }
+
+    /**
+     * @param bool $primaryKey
+     */
+    public function setPrimaryKey(bool $primaryKey): void
+    {
+        $this->primaryKey = $primaryKey;
+    }
+
+
+}
