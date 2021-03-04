619a5aabb0f7f6de6c0c0b5330f8c6303f8d2dcd
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:30:26 -0500

    New Img class for dynamic image retrieval

---
+++ app/controllers/Img.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use App\Api\Ad\ADUsers;
+use App\Models\Database\AppDatabase;
+use App\Models\District\DomainUser;
+use System\App\AppException;
+use System\App\Picture;
+use system\Header;
+
+class Img extends FileController
+{
+    public function index()
+    {
+        return $this->favicon();
+    }
+
+    public function favicon($requestedFavicon = null)
+    {
+        Header::sendFile(Picture::resize(AppDatabase::getAppIcon(), 64, 64), Header::IMAGE_MIXED);
+    }
+
+    public function logo($request)
+    {
+        $fileType = explode(".", $request)[1];
+        $resolution = explode("x", explode(".", $request)[0]);
+        $logo = Picture::resize(AppDatabase::getAppIcon(), $resolution[0], $resolution[1]);
+        Header::sendFile($logo, Header::IMAGE_MIXED);
+
+    }
+
+    public function user($username)
+    {
+        try {
+            $user = new DomainUser($username);
+        } catch (AppException $ex) {
+            Header::fileNotFound();
+        }
+        Header::sendFile($user->activeDirectory->getThumbnail(), Header::IMAGE_MIXED, true, $username . '.jpg');
+    }
+}
