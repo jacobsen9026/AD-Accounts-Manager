95aff3cc009f85901a1d07662c85be574a020646
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:12:43 -0500

    Removed div close for off navigation click detection.... need replacement Add timeout browser broadcasting

--- app/views/layouts/default_header.php
+++ app/views/layouts/default_header.php
@@ -1,46 +1,90 @@
 <?php
 
+use App\Models\Database\AppDatabase;
 use App\Models\Database\AuthDatabase;
+use App\Models\View\Javascript;
 use App\Models\View\Modal;
 
 echo $this->view("/layouts/head");
 ?>
 <script>
+    let sessionTimer = null;
+    let finalMinuteTimer = null;
+
     function init() {
-        return setTimeout(sessionExpiring, <?= (AuthDatabase::getSessionTimeout() - 60) * 1000 ?>)
+        startSessionTimer();
     }
 </script>
 
 <body class="bg-light"
       onload="init()">
-<div class="position-absolute w-100 h-100 p-0 m-0" onclick="$('#collapsibleNavbar').collapse('hide')">
-    <div class="container container-fluid centered px-3 px-md-5">
 
-        <?php
-        $sessionExpireModal = new Modal();
-        $sessionExpireModal->setTitle('Your session is about to expire')
-            ->setId('sessionExpirationModal')
-            ->setBody($this->modal('sessionExpiring'));
-        echo $sessionExpireModal->print();
+<div class="container container-fluid centered px-0 px-sm-3 px-md-5">
 
-        ?>
-        <script>
+    <?php
+    $sessionExpireModal = new Modal();
+    $sessionExpireModal->setTitle('Your session is about to expire')
+        ->setId('sessionExpirationModal')
+        ->setBody($this->modal('sessionExpiring'));
+    echo $sessionExpireModal->print();
 
-            function sessionExpiring() {
-                $("#<?=$sessionExpireModal->getId()?>").modal('show');
-                setTimeout(expireSession, 70 * 1000);
+    ?>
+    <script>
+        const channel = new BroadcastChannel('<?= AppDatabase::getAppAbbreviation() ?>-ADAMWebsite');
+
+        channel.postMessage('session_refreshed');
+
+        channel.addEventListener('message', (event) => {
+            switch (event.data) {
+                case 'session_expiring':
+                    console.log('Session expiring in another tab');
+                    $("#<?=$sessionExpireModal->getId()?>").modal('show');
+                    clearTimeout(finalMinuteTimer)
+                    finalMinuteTimer = setTimeout(expireSession, 70 * 1000);
+                    break;
+                case 'session_refreshed':
+                    console.log('Session refreshed from other tab');
+                    $("#sessionExpirationModal").modal("hide");
+                    startSessionTimer()
+                    break;
+                case 'session_expired':
+                    console.log('Session expired in another tab');
+                    window.location.href = window.location.pathname;
+                    break;
+
             }
+        });
 
-            function resetSession() {
-                $("#sessionExpirationModal").modal("hide");
-                return setTimeout(sessionExpiring, <?= (AuthDatabase::getSessionTimeout() - 60) * 1000 ?>)
-            }
 
-            function expireSession() {
-                window.location.href = "/logout";
-            }
-        </script>
-        <div class=' centered text-center text_centered container container-fluid py-5 px-5 mt-8 mb-5 mx-auto shadow-lg bg-white'>
+        function startSessionTimer() {
+            clearTimeout(sessionTimer);
+            clearTimeout(finalMinuteTimer);
+            <?=Javascript::debug('Starting session expiration timer: ' . (AuthDatabase::getSessionTimeout() - 60))?>
+            sessionTimer = setTimeout(sessionExpiring, <?= (AuthDatabase::getSessionTimeout() - 60) * 1000 ?>)
+        }
+
+        function sessionExpiring() {
+            channel.postMessage('session_expiring');
+            $("#<?=$sessionExpireModal->getId()?>").modal('show');
+            clearTimeout(finalMinuteTimer)
+            finalMinuteTimer = setTimeout(expireSession, 70 * 1000);
+        }
+
+
+        function resetSession() {
+
+            $("#sessionExpirationModal").modal("hide");
+            channel.postMessage('session_refreshed');
+            <?=Javascript::debug('Session refreshed')?>
+            startSessionTimer();
+        }
+
+        function expireSession() {
+            channel.postMessage('session_expired');
+            window.location.href = window.location.pathname;
+        }
+    </script>
+    <div class=' centered text-center text_centered container container-fluid py-5 px-1 px-md-5 mt-5 mt-md-8 mb-5 mx-auto shadow-lg bg-white'>
 
 
 
