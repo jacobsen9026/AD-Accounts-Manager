2903272cf2f7b508542edcaa7833c0359d8b6138
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:52:21 -0500

    Add error types

--- app/models/database/QueryError.php
+++ app/models/database/QueryError.php
@@ -7,4 +7,7 @@ namespace App\Models\Database;
 class QueryError
 {
     const NO_SUCH_COLUMN = 'no such column';
+    const NO_SUCH_TABLE = 'no such table';
+    const UNRECOGNIZED_TOKEN = 'unrecognized token';
+    const SYNTAX_ERROR = 'syntax error';
 }
