f71442a451e9f82abfd9f9eb07a0d2170c9ad49d
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:43:39 -0500

    New nofiticationdatabase class

---
+++ app/models/database/NotificationDatabase.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Models\Database;
+
+
+class NotificationDatabase extends DatabaseModel
+{
+    const TABLE_NAME = "Notification";
+
+    public static function get($id = null)
+    {
+        $response = parent::get($id);
+        if (is_null($response)) {
+            self::createTable();
+            $response = parent::get($id);
+
+        }
+        if ($id !== null) {
+            $response = $response[0];
+        }
+        return $response;
+    }
+
+    private static function createTable()
+    {
+        $query = new Query(self::TABLE_NAME, Query::CREATE);
+        $query->addSchema('ID', 'INTEGER', null, true, true)
+            ->addSchema('Name')
+            ->addSchema('Subject')
+            ->addSchema('Body');
+        return $query->run();
+    }
+
+    public static function create($newNotificationName)
+    {
+        return self::insertRow(["Name" => $newNotificationName]);
+    }
+
+    public static function updateTemplate($id, $name, $subject, $body)
+    {
+        return self::updateRow($id, ["Name" => $name, "Subject" => $subject, "Body" => $body]);
+    }
+
+    public static function deleteTemplate($id)
+    {
+        return self::deleteRow($id);
+    }
+
+
+}
