58390efda022dac4e7f3e176b026d19f936fcb53
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 11:01:42 -0500

    Add resize() Add getHex() Add overwrite()

--- system/models/post/UploadedFile.php
+++ system/models/post/UploadedFile.php
@@ -32,6 +32,7 @@ namespace System\Models\Post;
  * @author cjacobsen
  */
 
+use System\App\Picture;
 use System\File;
 use System\SystemLogger;
 
@@ -63,14 +64,10 @@ class UploadedFile
         return $this->name;
     }
 
-    public function getType()
+    public function setName($name)
     {
-        return $this->type;
-    }
-
-    public function getTempFileName()
-    {
-        return $this->tempFileName;
+        $this->name = $name;
+        return $this;
     }
 
     public function getError()
@@ -78,33 +75,15 @@ class UploadedFile
         return $this->error;
     }
 
-    public function getFileSize()
-    {
-        return $this->fileSize;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function setType($type)
-    {
-        $this->type = $type;
-        return $this;
-    }
-
-    public function setTempFileName($tempFileName)
-    {
-        $this->tempFileName = $tempFileName;
-        return $this;
-    }
-
     public function setError($error)
     {
         $this->error = $error;
         return $this;
+    }
+
+    public function getFileSize()
+    {
+        return $this->fileSize;
     }
 
     public function setFileSize($fileSize)
@@ -118,17 +97,76 @@ class UploadedFile
         move_uploaded_file($this->tempFileName, $destinationFilePath);
     }
 
-    public function getTempFileContents()
-    {
-        return File::getContents($this->tempFileName);
-    }
-
     public function exists()
     {
         if ($this->tempFileName == null) {
             return false;
         }
         return true;
+    }
+
+    public function getHex()
+    {
+        return bin2hex($this->getTempFileContents());
+    }
+
+    public function getTempFileContents()
+    {
+        return File::getContents($this->tempFileName);
+    }
+
+    public function resize(int $int)
+    {
+        switch ($this->getType()) {
+
+
+            case 'image/png':
+                $picture = imagecreatefrompng($this->getTempFileName());
+                break;
+            case 'image/jpeg':
+            case 'image/jpgx':
+            case 'image/jpg':
+                $picture = imagecreatefromjpeg($this->getTempFileName());
+                break;
+
+            case 'image/gif':
+                $picture = imagecreatefromgif($this->getTempFileName());
+                break;
+            case 'image/bmp':
+                $picture = imagecreatefrombmp($this->getTempFileName());
+                break;
+
+        }
+        $rawPicture = Picture::cropSquare($picture, $int);
+
+        $this->overwrite($rawPicture);
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getTempFileName()
+    {
+        return $this->tempFileName;
+    }
+
+    public function setTempFileName($tempFileName)
+    {
+        $this->tempFileName = $tempFileName;
+        return $this;
+    }
+
+    public function overwrite($rawPicture)
+    {
+        File::overwriteFile($this->getTempFileName(), $rawPicture);
     }
 
 }
