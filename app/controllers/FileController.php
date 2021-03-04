a8a562d4880aaebc3083a8624091bf52fa1888b5
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:20:38 -0500

    New parent class

---
+++ app/controllers/FileController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use App\App\App;
+
+class FileController extends Controller
+{
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+    }
+
+}
