1b6c301ab50c964dcc51545138adc981b0f4cdd1
Chris Jacobsen Work <cjacobsen@work.com>
2021-03-04 10:10:27 -0500

    Implemented get($id) from get() Added database response runtime caching

--- app/models/database/DatabaseModel.php
+++ app/models/database/DatabaseModel.php
@@ -38,17 +38,60 @@ use System\DatabaseLogger;
 abstract class DatabaseModel extends Model implements DatabaseModelInterface
 {
 
+    public static $cache = [];
 
     /**
-     * Returns the entire table
+     * Returns the entire table or a specific row by id
      *
-     * @return type
+     * @return array
      */
-    public static function get()
+    public static function get($id = null)
     {
-        $query = new Query(static::TABLE_NAME);
-        return $query->run();
+        if ($id === null) {
+            /**
+             * getting entire table
+             */
+            if (!is_array(self::$cache) ||
+                !key_exists(static::TABLE_NAME, self::$cache) ||
+                !key_exists('ENTIRE_TABLE', self::$cache[static::TABLE_NAME]) ||
+                is_null(self::$cache[static::TABLE_NAME]['ENTIRE_TABLE'])) {
+                /**
+                 * No cached entry found running new database call
+                 */
+                $query = new Query(static::TABLE_NAME);
+                $response = $query->run();
+                DatabaseLogger::get()->info('Storing entire table to cache for ' . static::TABLE_NAME);
+                self::$cache[static::TABLE_NAME]['ENTIRE_TABLE'] = $response;
+                return $response;
+            } else {
+                DatabaseLogger::get()->info('Retreiving entire table from cache for ' . static::TABLE_NAME . ': ' . self::$cache[static::TABLE_NAME]['ENTIRE_TABLE']);
+                return self::$cache[static::TABLE_NAME]['ENTIRE_TABLE'];
+            }
+        } else {
+            /**
+             * getting specific row
+             */
+            if (!is_array(self::$cache) ||
+                !key_exists(static::TABLE_NAME, self::$cache) ||
+                !key_exists($id, self::$cache[static::TABLE_NAME]) ||
+                is_null(self::$cache[static::TABLE_NAME][$id])) {
+                /**
+                 * No cached entry found running new database call
+                 */
+                $query = new Query(static::TABLE_NAME);
+                $query->where('ID', $id);
+                $response = $query->run();
+                DatabaseLogger::get()->info('Storing entire row to cache for ' . static::TABLE_NAME . '.' . $id);
+                self::$cache[static::TABLE_NAME][$id] = $response;
+                return $response;
+            } else {
+                DatabaseLogger::get()->info('Retreiving entire row from cache for ' . static::TABLE_NAME . '.' . $id . ': ' . self::$cache[static::TABLE_NAME][$id]);
+                return self::$cache[static::TABLE_NAME][$id];
+            }
+        }
+
     }
+
 
     /**
      * The calling object MUST have a TABLE_NAME constant
@@ -65,6 +108,23 @@ abstract class DatabaseModel extends Model implements DatabaseModelInterface
         $query = new Query(static::TABLE_NAME, Query::UPDATE, $column);
         $query->where("ID", 1)
             ->set($column, $value);
+        DatabaseLogger::get()->info('Storing value to cache for ' . $column . ': ' . $value);
+        self::$cache[static::TABLE_NAME][1][$column] = $value;
+        $return = $query->run();
+        if ($query->hadError()) {
+            self::$cache[static::TABLE_NAME][1][$column] = null;
+            if ($query->getError() == QueryError::NO_SUCH_COLUMN) {
+                self::addColumn(static::TABLE_NAME, $column);
+                $return = $query->run();
+
+            }
+        }
+        return $return;
+    }
+
+    protected static function addColumn($table, string $column)
+    {
+        $query = new Query($table, Query::ALTER, $column);
         return $query->run();
     }
 
@@ -80,16 +140,96 @@ abstract class DatabaseModel extends Model implements DatabaseModelInterface
      */
     protected static function getDatabaseValue($column, $id = 1)
     {
-        $query = new Query(static::TABLE_NAME, Query::SELECT, $column);
-        $query->where("ID", $id);
-        return $query->run();
+
+        if (!is_array(self::$cache) ||
+            !key_exists(static::TABLE_NAME, self::$cache) ||
+            !key_exists($id, self::$cache[static::TABLE_NAME]) ||
+            !is_array(self::$cache[static::TABLE_NAME][$id]) ||
+            !key_exists($column, self::$cache[static::TABLE_NAME][$id]) ||
+            is_null(self::$cache[static::TABLE_NAME][$id][$column])) {
+            $query = new Query(static::TABLE_NAME, Query::SELECT, $column);
+            $query->where("ID", $id);
+            $response = $query->run();
+            if (!$query->hadError()) {
+                /*
+                 * Query ran with no errors
+                 */
+
+                if (is_string($response) && strlen($response) < 1000) {
+                    DatabaseLogger::get()->info('Storing value to cache for ' . $column . ': ' . $response);
+                }
+                self::$cache[static::TABLE_NAME][$id][$column] = $response;
+                return $response;
+            } else {
+                /*
+                 * Query ran into errors while running
+                 */
+                switch ($query->getError()) {
+                    case QueryError::NO_SUCH_COLUMN:
+                        /*
+                         * The requested column did not exist
+                         */
+                        DatabaseLogger::get()->error(QueryError::NO_SUCH_COLUMN);
+                        break;
+                    case QueryError::NO_SUCH_TABLE:
+                        DatabaseLogger::get()->error(QueryError::NO_SUCH_TABLE);
+                        break;
+                    default:
+                        DatabaseLogger::get()->error('Other error');
+                        break;
+                }
+            }
+        } else {
+            if (strlen(self::$cache[static::TABLE_NAME][$id][$column]) < 1000) {
+                DatabaseLogger::get()->info('Retreiving value from cache for ' . $column . ': ' . self::$cache[static::TABLE_NAME][$id][$column]);
+            } else {
+                DatabaseLogger::get()->info('Retreiving value from cache for ' . $column);
+            }
+            return self::$cache[static::TABLE_NAME][$id][$column];
+        }
     }
 
-    private static function columnExists($tableName, string $column)
+    protected static function insertRow($data = null, $id = null)
     {
-        $query = new Query($tableName, Query::SHOW, $column);
-        DatabaseLogger::get()->info($query->run());
-        return $query->run();
+        $query = new Query(static::TABLE_NAME, Query::INSERT);
+        if ($data !== null && is_array($data) && !empty($data)) {
+            foreach ($data as $column => $value) {
+                $query->insert($column, $value);
+            }
+        }
+        if ($id !== null) {
+
+
+            $query->insert('ID', $id);
+        } else {
+            $id = 1;
+        }
+
+        $response = $query->run();
+        self::$cache[static::TABLE_NAME][$id] = null;
+        //DatabaseLogger::get()->error($response);
+
     }
+
+    protected static function updateRow($id, $data)
+    {
+        if (is_array($data) && !empty($data)) {
+            $query = new Query(static::TABLE_NAME, Query::UPDATE);
+            foreach ($data as $key => $value) {
+                $query->set($key, $value);
+            }
+            $query->where('ID', $id);
+            $query->run();
+        }
+    }
+
+    protected static function deleteRow($id)
+    {
+        $query = new Query(static::TABLE_NAME, Query::DELETE);
+        $query->where('ID', $id);
+        $query->run();
+
+    }
+
 
 }
