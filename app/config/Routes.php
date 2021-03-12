91953b9d78ba005c3a19afb3154940bee89c173a
jacobsen9026 <jacobsen9026@gmail.com>
2021-03-12 15:14:41 -0500

    Remove obsolete routes

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -11,14 +11,5 @@
 $this->customRoutes[] = ["install", "index", "home", "index"];
 $this->customRoutes[] = ["Favicon_ico", "index", "Img", "favicon"];
 
-/*
- * Settings routes
- */
-/*
-$this->customRoutes[] = ["Districts", "*", "settings\Districts", "*"];
-$this->customRoutes[] = ["Schools", "*", "settings\Schools", "*"];
-$this->customRoutes[] = ["Grades", "*", "settings\Grades", "*"];
-$this->customRoutes[] = ["Departments", "*", "settings\Departments", "*"];
-$this->customRoutes[] = ["Teams", "*", "settings\Teams", "*"];
-*/
+
 
