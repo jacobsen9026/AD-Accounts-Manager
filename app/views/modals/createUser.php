aa8c1df130c0ad771a96ea56c09380fa3023baba
jacobsen9026 <jacobsen9026@gmail.com>
2021-03-12 14:30:23 -0500

    Add tabs and more new user inputs

--- app/views/modals/createUser.php
+++ app/views/modals/createUser.php
@@ -3,6 +3,8 @@
 use App\Models\View\Javascript;
 use App\Forms\FormText;
 use System\App\Forms\FormButton;
+use System\App\Forms\FormDropdown;
+use System\App\Forms\FormDropdownOption;
 use System\App\Forms\FormElementGroup;
 use System\App\Forms\Form;
 use System\Lang;
@@ -28,6 +30,23 @@ $password->medium()
     ->isPassword();
 
 $showPassword = new FormButton('showPass');
+
+$logonScript = new FormText(Lang::get('Logon Script'), '', 'logonscript');
+$logonScript->medium();
+
+
+$homeDrive = new FormDropdown(Lang::get('Home Drive'), '', 'homedrive');
+$homeDrive->medium();
+foreach (range('B', 'Z') as $letter) {
+    $option = new FormDropdownOption($letter . ":", $letter);
+    //$option->addInputClasses('text-center');
+    $homeDrive->addOption($option);
+}
+
+
+$homePath = new FormText(Lang::get('Home Path'), '', 'homepath');
+$homePath->medium();
+
 $showPassword->setType('button')
     ->setInputClasses('h-100')
     ->setName('<i class="fas fa-eye-slash"></i>');
@@ -64,9 +83,48 @@ $form->addElementToNewRow($fname)
     ->addElementToNewRow($email)
     ->addElementToNewRow($ouInput)
     ->addElementToNewRow($button);
-echo $form->print();
+//echo $form->print();
+?>
+<form action="/users/create" method="post" name="UserCreate" id="UserCreateForm">
 
-
-
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#general" role="tab" aria-controls="home"
+               aria-selected="true">General</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="address-tab" data-toggle="tab" href="#address" role="tab" aria-controls="Address"
+               aria-selected="false">Address</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account"
+               aria-selected="false">Account</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
+            <?php
+            echo $fname->print();
+            echo $mname->print();
+            echo $lname->print();
+            echo $fullname->print();
+            echo $description->print();
+            ?>
+        </div>
+        <div class="tab-pane fade" id="address" role="tabpanel" aria-labelledby="address-tab">......</div>
+        <div class="tab-pane fade" id="account" role="tabpanel" aria-labelledby="account-tab">
+            <?php
+            echo $logon->print();
+            echo $passwordInput->print();
+            echo $email->print();
+            echo $ouInput->print();
+            echo $logonScript->print();
+            echo $homeDrive->print();
+            echo $homePath->print();
+            ?>
+        </div>
+    </div>
+    <?= $button->print(); ?>
+</form>
 
 
