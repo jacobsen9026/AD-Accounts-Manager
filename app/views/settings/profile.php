cd28c092427cdf33ea08c8d80774b77344e6a31b
jacobsen9026 <jacobsen9026@gmail.com>
2021-03-12 15:12:50 -0500

    Add user email and notification options inputs

--- app/views/settings/profile.php
+++ app/views/settings/profile.php
@@ -7,6 +7,7 @@ use System\App\Forms\FormDropdownOption;
 use System\App\Forms\FormButton;
 use App\Models\View\Javascript;
 use App\Models\User\User;
+use System\App\Forms\FormSlider;
 
 ?>
 <div>
@@ -32,7 +33,17 @@ use App\Models\User\User;
         }
         $themeDropDown->addOption($option);
     }
-
+    $userEmail = new FormText('Email Address', '', 'email', $user->getEmail());
+    $notifyUserChange = new FormSlider('Notify on user changes', '', 'notify_user_changes', (int)$user->getNotificationOptions()->isUserChange());
+    $notifyUserChange->small();
+    $notifyUserDisable = new FormSlider('Notify on user enable/disable', '', 'notify_user_enable', (int)$user->getNotificationOptions()->isUserDisable());
+    $notifyUserDisable->small();
+    $notifyUserCreation = new FormSlider('Notify on user creation', '', 'notify_user_create', (int)$user->getNotificationOptions()->isUserCreate());
+    $notifyUserCreation->small();
+    $notifyGroupChange = new FormSlider('Notify on group changes', '', 'notify_group_change', (int)$user->getNotificationOptions()->isGroupChange());
+    $notifyGroupChange->small();
+    $notifyGroupCreation = new FormSlider('Notify on group creation', '', 'notify_group_create', (int)$user->getNotificationOptions()->isGroupCreate());
+    $notifyGroupCreation->small();
     $apiKey = new FormText('API Key', '', 'apiKey', $user->getApiToken());
     $apiKey->disable()
         ->setId('apiKey')
@@ -52,7 +63,12 @@ use App\Models\User\User;
         ->setSubLabel("Updating will overwrite existing API key. You're old key will no longer work.");
 
     $profileForm->addElementToNewRow($themeDropDown)
-        //     ->addElementToNewRow($privilegeLevel)
+        ->addElementToNewRow($userEmail)
+        ->addElementToNewRow($notifyUserChange)
+        ->addElementToCurrentRow($notifyUserDisable)
+        ->addElementToCurrentRow($notifyUserCreation)
+        ->addElementToCurrentRow($notifyGroupChange)
+        ->addElementToCurrentRow($notifyGroupCreation)
         ->addElementToNewRow($apiKey)
         ->addElementToNewRow($updateAPIKey)
         ->addElementToNewRow($saveButton);
