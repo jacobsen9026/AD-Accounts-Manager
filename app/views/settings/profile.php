7b8d56d3bfcc78173065b90609a3bc9d58ec8e2c
Chris Jacobsen Work <cjacobsen@work.com>
2021-07-12 12:06:19 -0400

    Improve user notification options layout Add notifyAll slider

--- app/views/settings/profile.php
+++ app/views/settings/profile.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Config\Theme;
 use App\Forms\FormText;
 use System\App\Forms\Form;
 use System\App\Forms\FormDropdown;
@@ -7,6 +8,7 @@ use System\App\Forms\FormDropdownOption;
 use System\App\Forms\FormButton;
 use App\Models\View\Javascript;
 use App\Models\User\User;
+use System\App\Forms\FormHTML;
 use System\App\Forms\FormSlider;
 
 ?>
@@ -24,7 +26,7 @@ use System\App\Forms\FormSlider;
     $profileForm = new Form('/settings/profile');
     $themeDropDown = new FormDropdown("Theme", 'Set display theme', "theme");
     //$themeDropDown->medium();
-    foreach (app\config\Theme::getThemes() as $theme) {
+    foreach (Theme::getThemes() as $theme) {
         $option = new FormDropdownOption($theme, $theme);
 
 
@@ -34,16 +36,29 @@ use System\App\Forms\FormSlider;
         $themeDropDown->addOption($option);
     }
     $userEmail = new FormText('Email Address', '', 'email', $user->getEmail());
-    $notifyUserChange = new FormSlider('Notify on user changes', '', 'notify_user_changes', (int)$user->getNotificationOptions()->isUserChange());
-    $notifyUserChange->small();
-    $notifyUserDisable = new FormSlider('Notify on user enable/disable', '', 'notify_user_enable', (int)$user->getNotificationOptions()->isUserDisable());
-    $notifyUserDisable->small();
-    $notifyUserCreation = new FormSlider('Notify on user creation', '', 'notify_user_create', (int)$user->getNotificationOptions()->isUserCreate());
-    $notifyUserCreation->small();
-    $notifyGroupChange = new FormSlider('Notify on group changes', '', 'notify_group_change', (int)$user->getNotificationOptions()->isGroupChange());
-    $notifyGroupChange->small();
-    $notifyGroupCreation = new FormSlider('Notify on group creation', '', 'notify_group_create', (int)$user->getNotificationOptions()->isGroupCreate());
-    $notifyGroupCreation->small();
+    $notifyTitle = new FormHTML('Notify on');
+    $notifyAll = new FormSlider('All', '', 'notify_all', (int)$user->getNotificationOptions()->isAll());
+    $js = Javascript::onChange($notifyAll->getId(), '$(".notification-slider").val(e.target.value)');
+    $notifyAll->small()
+        ->setScript($js);
+    $notifyUserChange = new FormSlider('User changes', '', 'notify_user_changes', (int)$user->getNotificationOptions()->isUserChange());
+    $notifyUserChange->small()
+        ->addInputClasses('notification-slider');
+    $notifyUserDisable = new FormSlider('User enable/disable', '', 'notify_user_enable', (int)$user->getNotificationOptions()->isUserDisable());
+    $notifyUserDisable->small()
+        ->addInputClasses('notification-slider');
+
+    $notifyUserCreation = new FormSlider('User creation', '', 'notify_user_create', (int)$user->getNotificationOptions()->isUserCreate());
+    $notifyUserCreation->small()
+        ->addInputClasses('notification-slider');
+
+
+    $notifyGroupChange = new FormSlider('Group changes', '', 'notify_group_change', (int)$user->getNotificationOptions()->isGroupChange());
+    $notifyGroupChange->small()
+        ->addInputClasses('notification-slider');
+    $notifyGroupCreation = new FormSlider('Group creation', '', 'notify_group_create', (int)$user->getNotificationOptions()->isGroupCreate());
+    $notifyGroupCreation->small()
+        ->addInputClasses('notification-slider');
     $apiKey = new FormText('API Key', '', 'apiKey', $user->getApiToken());
     $apiKey->disable()
         ->setId('apiKey')
@@ -64,11 +79,13 @@ use System\App\Forms\FormSlider;
 
     $profileForm->addElementToNewRow($themeDropDown)
         ->addElementToNewRow($userEmail)
+        ->addElementToNewRow($notifyTitle)
         ->addElementToNewRow($notifyUserChange)
         ->addElementToCurrentRow($notifyUserDisable)
         ->addElementToCurrentRow($notifyUserCreation)
         ->addElementToCurrentRow($notifyGroupChange)
         ->addElementToCurrentRow($notifyGroupCreation)
+        ->addElementToCurrentRow($notifyAll)
         ->addElementToNewRow($apiKey)
         ->addElementToNewRow($updateAPIKey)
         ->addElementToNewRow($saveButton);
