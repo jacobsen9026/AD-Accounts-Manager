a7d25583b429a37744b6114e9f2960a9b0204ed2
Chris Jacobsen Work <cjacobsen@work.com>
2021-07-12 11:24:36 -0400

    Add log leve array

--- system/log/CommonLogLevel.php
+++ system/log/CommonLogLevel.php
@@ -9,4 +9,7 @@ use Psr\Log\LogLevel;
 class CommonLogLevel extends LogLevel
 {
 
+    const NUMERIC_VALUE = ["debug" => 7, "info" => 6, "notice" => 5, "warning" => 4, "error" => 3, "critical" => 2, "alert" => 1, "emergency" => 0];
+
+
 }
