17aebe4ec329e306af6c53fc72342b53f67465a2
Chris Jacobsen Work <cjacobsen@work.com>
2021-07-12 12:01:37 -0400

    Add other js file references

--- app/controllers/Js.php
+++ app/controllers/Js.php
@@ -5,6 +5,9 @@ namespace App\Controllers;
 
 
 use App\Models\Database\AppDatabase;
+use App\Models\Database\AuthDatabase;
+use App\Models\View\Javascript;
+use System\File;
 use system\Header;
 
 class Js extends FileController
@@ -100,5 +103,124 @@ self.addEventListener('fetch', event => {
         ";
         Header::sendFile($serviceWorker, Header::APPLICATION_JAVASCRIPT);
         exit;
+    }
+
+    public function jquery_redirect_js()
+    {
+        Header::sendFile(File::getContents(ROOTPATH . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'jquery.redirect.js'), Header::APPLICATION_JAVASCRIPT);
+    }
+
+    public function jqueryUi_js()
+    {
+        Header::sendFile(File::getContents(ROOTPATH . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'jquery-ui.js'), Header::APPLICATION_JAVASCRIPT);
+    }
+
+    public function sessionManager_js()
+    {
+        $script = ' 
+        if(typeof channel === "undefined"){
+        console.log("Create channel");
+            channel = new BroadcastChannel("' . AppDatabase::getAppAbbreviation() . '-ADAMWebsite");
+            console.log(channel);
+        }
+        channel.postMessage("session_refreshed");
+
+        channel.addEventListener("message", (event) => {
+            switch (event.data) {
+                case "session_expiring":
+                    console.log("Session expiring in another tab");
+                    $("#sessionExpirationModal").modal("show");
+                    clearTimeout(finalMinuteTimer)
+                    finalMinuteTimer = setTimeout(expireSession, 70 * 1000);
+                    break;
+                case "session_refreshed":
+                    console.log("Session refreshed from other tab");
+                    $("#sessionExpirationModal").modal("hide");
+                    startSessionTimer()
+                    break;
+                case "session_expired":
+                    console.log("Session expired in another tab");
+                    window.location.href = window.location.pathname;
+                    break;
+
+            }
+        });
+
+
+        function startSessionTimer() {
+            clearTimeout(sessionTimer);
+            clearTimeout(finalMinuteTimer);
+            ' . Javascript::debug("Starting session expiration timer: " . (AuthDatabase::getSessionTimeout() - 60)) . '
+            sessionTimer = setTimeout(sessionExpiring, ' . (AuthDatabase::getSessionTimeout() - 60) * 1000 . ')
+        }
+
+        function sessionExpiring() {
+            channel.postMessage("session_expiring");
+            $("#sessionExpirationModal").modal("show");
+            clearTimeout(finalMinuteTimer)
+            finalMinuteTimer = setTimeout(expireSession, 70 * 1000);
+        }
+
+
+        function resetSession() {
+
+            $("#sessionExpirationModal").modal("hide");
+            channel.postMessage("session_refreshed");
+            ' . Javascript::debug("Session refreshed") . '
+            startSessionTimer();
+        }
+
+        function expireSession() {
+            channel.postMessage("session_expired");
+            window.location.href = window.location.pathname;
+        }
+        ';
+        Header::sendFile($script, Header::APPLICATION_JAVASCRIPT);
+    }
+
+    public function touchPunch_js()
+    {
+        Header::sendFile('
+        //TouchPunch
+        !function (a) {
+            function f(a, b) {
+                if (!(a.originalEvent.touches.length > 1)) {
+                    a.preventDefault();
+                    var c = a.originalEvent.changedTouches[0], d = document.createEvent("MouseEvents");
+                    d.initMouseEvent(b, !0, !0, window, 1, c.screenX, c.screenY, c.clientX, c.clientY, !1, !1, !1, !1, 0, null),
+                        a.target.dispatchEvent(d)
+                }
+            }
+
+            if (a.support.touch = "ontouchend" in document, a.support.touch) {
+                var e, b = a.ui.mouse.prototype, c = b._mouseInit, d = b._mouseDestroy;
+                b._touchStart = function (a) {
+                    var b = this;
+                    !e && b._mouseCapture(a.originalEvent.changedTouches[0]) && (e = !0, b._touchMoved = !1, f(a, "mouseover"), f(a,
+                        "mousemove"), f(a, "mousedown"))
+                }, b._touchMove = function (a) {
+                    e && (this._touchMoved = !0, f(a, "mousemove"))
+                }, b._touchEnd = function (a) {
+                    e && (f(a, "mouseup"), f(a, "mouseout"), this._touchMoved || f(a, "click"), e = !1)
+                }, b._mouseInit = function () {
+                    var b = this;
+                    b.element.bind({
+                        touchstart: a.proxy(b, "_touchStart"),
+                        touchmove: a.proxy(b, "_touchMove"),
+                        touchend: a.proxy(b, "_touchEnd")
+                    }), c.call(b)
+                }, b._mouseDestroy = function () {
+                    var b = this;
+                    b.element.unbind({
+                        touchstart: a.proxy(b, "_touchStart"),
+                        touchmove: a.proxy(b, "_touchMove"),
+                        touchend: a.proxy(b, "_touchEnd")
+                    }), d.call(b)
+                }
+            }
+        }(jQuery);
+        //End TouchPunch
+
+', Header::APPLICATION_JAVASCRIPT);
     }
 }
